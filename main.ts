4a63968e1eacec3695c4317ebd888fb6ed5a3a21
Aryan Jand <97502222+aryanjand@users.noreply.github.com>
2023-04-30 20:23:31 -0700

    The modification should now make it run for windows as well
    Just problems with file path. Also, only https://localhost:8000/ works not http://localhost:8000/. (https vs http)

--- main.ts
+++ main.ts
@@ -7,20 +7,27 @@
 import { ServerContext, start } from "$fresh/server.ts";
 import { serveTls } from "std/http/server.ts";
 import manifest from "./fresh.gen.ts";
+import * as path from "std/path/mod.ts";
 
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
 const port = parseInt(Deno.env.get("PORT") || "8000", 10);
+// There is a spical character at Index 0.
+const filePathUrl = new URL(".", import.meta.url).pathname;
+const __dirname = filePathUrl.substring(1);
+const certFile = path.join(__dirname, "local-certs", "localhost.crt");
+const keyFile = path.join(__dirname, "local-certs", "localhost.key");
 
 const opts = { port, plugins: [twindPlugin(twindConfig)] };
+const self = Deno.env.get("SELF_URL");
 
-if (Deno.env.get("SELF_URL") === "https://localhost:8000") {
+if (self === "https://localhost:8000") {
   const ctx = await ServerContext.fromManifest(manifest, opts);
   await serveTls(ctx.handler(), {
     ...opts,
-    certFile: "./local-certs/localhost.crt",
-    keyFile: "./local-certs/localhost.key",
+    certFile,
+    keyFile,
   });
 } else {
   await start(manifest, opts);
