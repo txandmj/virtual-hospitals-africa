fd4237ad6465c3263d60d62e9a96ff89c86b8b29
Will Weiss <will.weiss1230@classdojo.com>
2023-05-01 14:34:39 -0400

    patient reset works

--- routes/patient-reset.ts
+++ routes/patient-reset.ts
@@ -3,9 +3,19 @@ import { WithSession } from "fresh_session";
 import { remove } from "../models/patients.ts";
 
 export const handler: Handlers<Record<string, never>, WithSession> = {
-  async GET(_req, ctx) {
-    const { phone_number } = ctx.params;
-    await remove({ phone_number });
-    return new Response("Everything reset", { status: 200 });
+  async GET(req, ctx) {
+    const phone_number = new URL(req.url).searchParams.get("phone_number");
+    if (!phone_number) {
+      return new Response("Please provide phone_number in query params", {
+        status: 400,
+      });
+    }
+    try {
+      await remove({ phone_number });
+    } catch (err) {
+      console.error(err);
+      return new Response("Error removing patient", { status: 500 });
+    }
+    return new Response("Patient removed", { status: 200 });
   },
 };
