4fa59d465baae21659caaf156c93aded90d75846
Chun <peterpang1103@gmail.com>
2023-05-04 14:14:17 -0700

    uncomment code to check for bugs

--- chatbot/conversationStates.ts
+++ chatbot/conversationStates.ts
@@ -263,28 +263,27 @@ const conversationStates: {
       patientMessage: UnhandledPatientMessage,
     ): Promise<UnhandledPatientMessage> {
       console.log(
-        "onboarded:make_appointment:other_scheduling_options onnEnter",
+        "onboarded:make_appointment:other_scheduling_options onEnter",
       );
-
       // Created new function to update the row in the db, we get the row id by using the patientMessage that was modified in the previous state.
-      // const declinedOfferedTime = await appointments.declineOfferedTime(
-      //   trx,
-      //   { id: patientMessage.appointment_offered_times[0]?.id ?? 0 }, //trying to hardcode 0 to id if it's undefined.
-      // );
-      // console.log("DeclinedOfferedTime", declinedOfferedTime);
+      const declinedOfferedTime = await appointments.declineOfferedTime(
+        trx,
+        { id: patientMessage.appointment_offered_times[0]?.id ?? 0 }, //trying to hardcode 0 to id if it's undefined.
+      );
+      console.log("DeclinedOfferedTime", declinedOfferedTime);
       // I think we are getting the error below because of null safty.
       // It could be beacuse the delineoffer is never null
 
-      // const declined: ReturnedSqlRow<
-      //   AppointmentOfferedTime & { doctor_name: string }
-      // >[] = [
-      //   declinedOfferedTime,
-      //   ...compact(patientMessage.appointment_offered_times),
-      // ];
+      const declined: ReturnedSqlRow<
+        AppointmentOfferedTime & { doctor_name: string }
+      >[] = [
+        declinedOfferedTime,
+        ...compact(patientMessage.appointment_offered_times),
+      ];
 
       return {
         ...patientMessage,
-        // appointment_offered_times: declined,
+        appointment_offered_times: declined,
       };
     },
 
