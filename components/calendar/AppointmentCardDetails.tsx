95dffac14ecdcd0e56f7bc1fdc1e013d6deae160
syte1 <belalk@live.ca>
2023-05-12 12:56:30 -0700

    match card

--- components/calendar/AppointmentCardDetails.tsx
+++ components/calendar/AppointmentCardDetails.tsx
@@ -20,16 +20,20 @@ const AppointmentCardDetails: FunctionComponent<AppointmentCardProps> = ({
   return (
     <div className="relative flex border border-gray-300 rounded shadow-lg overflow-hidden">
       <div className={`w-6 h-full ${stripeColor} absolute`}></div>
-      <div className="pl-10 p-2 space-y-1">
-        <div className="font-semibold text-lg">{time}</div>
-        <div className="font-semibold text-base">{patientName}</div>
-        <div className="text-base">Age: {patientAge}</div>
-        <div className="text-base pb-8">{clinicName}</div>
+      <div className="pl-10 p-3 space-y-2">
+        <div className="font-semibold text-2xl text-green-700">{time}</div>
+        <div className="font-semibold text-xl">{patientName}</div>
+        <div className="text-xl text-gray-500">
+          Age: {patientAge}
+        </div>
+        <div className="text-xl pb-8 text-gray-500">
+          {clinicName}
+        </div>
       </div>
-      <div className="px-1 absolute text-green-900 font-bold bg-green-100 top-2 right-2 text-base font-semibold">
+      <div className="px-1 absolute text-green-700 font-bold text-lg bg-blue-50 top-2 right-2">
         {durationMinutes}
       </div>
-      <div className="absolute bottom-6 right-3 text-lg bg-green-600 text-white py-2 px-6 rounded shadow-lg font-normal mb-2">
+      <div className="cursor-pointer absolute bottom-6 right-3 text-lg bg-green-600 text-white py-3 px-6 rounded shadow-lg font-normal mb-2">
         VIEW
       </div>
     </div>
