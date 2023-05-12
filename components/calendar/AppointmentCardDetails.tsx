23157feab6c1dbc9327ebd3f853ac9c8e644ad90
syte1 <belalk@live.ca>
2023-05-12 13:08:57 -0700

    adjust font

--- components/calendar/AppointmentCardDetails.tsx
+++ components/calendar/AppointmentCardDetails.tsx
@@ -21,7 +21,7 @@ const AppointmentCardDetails: FunctionComponent<AppointmentCardProps> = ({
     <div className="relative flex border border-gray-300 rounded shadow-lg overflow-hidden">
       <div className={`w-6 h-full ${stripeColor} absolute`}></div>
       <div className="pl-10 p-3 space-y-2">
-        <div className="font-semibold text-2xl text-green-700">{time}</div>
+        <div className="font-semibold text-2xl text-green-600">{time}</div>
         <div className="font-semibold text-xl">{patientName}</div>
         <div className="text-xl text-gray-500">
           Age: {patientAge}
@@ -30,7 +30,7 @@ const AppointmentCardDetails: FunctionComponent<AppointmentCardProps> = ({
           {clinicName}
         </div>
       </div>
-      <div className="px-1 absolute text-green-700 font-bold text-lg bg-blue-50 top-2 right-2">
+      <div className="px-1 absolute text-green-700 font-bold text-xl bg-blue-50 top-2 right-2">
         {durationMinutes}
       </div>
       <div className="cursor-pointer absolute bottom-6 right-3 text-lg bg-green-600 text-white py-3 px-6 rounded shadow-lg font-normal mb-2">
