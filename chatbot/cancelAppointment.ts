de93101ad6fd708d50607f90140373cc9d7dbbb7
Chun <peterpang1103@gmail.com>
2023-05-15 16:50:28 -0700

    fixing deno lints

--- chatbot/cancelAppointment.ts
+++ chatbot/cancelAppointment.ts
@@ -1,7 +1,7 @@
 import { TrxOrDb, UnhandledPatientMessage } from "../types.ts";
 import { assert } from "std/testing/asserts.ts";
 import * as google from "../external-clients/google.ts";
-import { getAllWithTokens, getWithTokensById } from "../models/doctors.ts";
+import { getWithTokensById } from "../models/doctors.ts";
 import { appointmentDetails } from "./makeAppointment.ts";
 
 
