715a60f92a9a0dd0f9f0760f1faeb44730ff7387
Will Weiss <will.weiss1230@classdojo.com>
2023-05-19 21:07:36 -0400

    simplify creation of migration & add postgis, and clinics tables

--- migrate-db/migrate-db.ts
+++ migrate-db/migrate-db.ts
@@ -1,31 +1,42 @@
-import { Migrator } from "kysely";
+import { assert } from "std/_util/asserts.ts";
+import { Migrator, Migration } from "kysely";
 import db from "../external-clients/db.ts";
-import * as _20230126000000_add_patients_table from "./migrations/20230126000000_add_patients_table.ts";
-import * as _20230127024002_add_whatsapp_conversation_tables from "./migrations/20230127024002_add_whatsapp_conversation_tables.ts";
-import * as _20230127203321_add_doctors_appointments from "./migrations/20230127203321_add_doctors_appointments.ts";
-import * as _20230316155657_add_doctor_google_tokens from "./migrations/20230316155657_add_doctor_google_tokens.ts";
-import * as _20230517124053_add_update_trigger from "./migrations/20230517124053_add_update_trigger.ts";
-import * as _20230518122050_add_appointment_status from "./migrations/20230518122050_add_appointment_status.ts";
 
+if (!Deno.args.length) {
+  console.error("Please provide a migration name as in\ndeno task migrate:create name");
+  Deno.exit(1);
+}
 
-async function migrateToLatest() {
-  const migrator = new Migrator({
-    db,
-    provider: {
-      getMigrations() {
-        return Promise.resolve({
-          _20230126000000_add_patients_table,
-          _20230127024002_add_whatsapp_conversation_tables,
-          _20230127203321_add_doctors_appointments,
-          _20230316155657_add_doctor_google_tokens,
-          _20230517124053_add_update_trigger,
-          _20230518122050_add_appointment_status,
-        });
-      },
-    },
-  });
+const migrations: Record<string, Migration> = {};
+for (const migrationFile of Deno.readDirSync("./migrate-db/migrations")) {
+  const migrationName = migrationFile.name
+  const migration = await import(`./migrations/${migrationName}`);
+  migrations[migrationName] = migration;
+}
 
-  const { error, results } = await migrator.migrateToLatest();
+const migrator = new Migrator({
+  db,
+  provider: {
+    getMigrations: () => Promise.resolve(migrations),
+  },
+});
+
+function startMigrating() {
+  switch (Deno.args[0]) {
+    case "--up":
+      return migrator.migrateToLatest();
+    case "--down":
+      return migrator.migrateDown();
+    case "--to":
+      assert(Deno.args[1], "Must specify target");
+      return migrator.migrateTo(Deno.args[1]);
+    default:
+      throw new Error("Invalid command");
+  }
+}
+
+async function migrate() {
+  const { error, results } = await startMigrating()
 
   results?.forEach((it) => {
     if (it.status === "Success") {
@@ -35,12 +46,12 @@ async function migrateToLatest() {
     }
   });
 
+  await db.destroy();
+
   if (error) {
     console.error("failed to migrate");
     throw error;
   }
-
-  await db.destroy();
 }
 
-migrateToLatest();
+migrate();
--- migrate-db/migrations/20230517124053_add_update_trigger.ts
+++ migrate-db/migrations/20230517124053_add_update_trigger.ts
@@ -1,16 +1,21 @@
 import { Kysely, sql } from "kysely";
 
+
+export async function selectAllNonKyselyMetaTables(db: Kysely<unknown>): Promise<string[]> {
+  const tables = await sql<{table_name: string}>`
+    SELECT table_name
+      FROM information_schema.tables
+     WHERE table_schema = 'public'
+       AND table_type = 'BASE TABLE'
+       AND table_name NOT LIKE 'kysely_%'
+  `.execute(db)
+
+  return tables.rows.map(({ table_name }) => table_name);
+}
+
 export async function up(db: Kysely<unknown>) {
-  const tables = await sql<{table_name: string}>
-  `SELECT table_name
-  FROM information_schema.tables
-  WHERE table_schema = 'public'
-    AND table_type = 'BASE TABLE'`
-    .execute(db)
-    
-  for (const {table_name} of tables.rows){
-      await sql
-      `CREATE OR REPLACE FUNCTION update_updated_at()
+  await sql`
+    CREATE OR REPLACE FUNCTION update_updated_at()
       RETURNS TRIGGER AS $$ 
       BEGIN
       NEW.updated_at = NOW();
@@ -18,27 +23,26 @@ export async function up(db: Kysely<unknown>) {
       END;
       $$
       LANGUAGE plpgsql;
-      `.execute(db)
-      await sql`
+  `.execute(db)
+
+  const tables = await selectAllNonKyselyMetaTables(db)
+  
+  for (const table of tables) {
+    await sql`
       CREATE TRIGGER update_updated_at_trigger
-            BEFORE UPDATE ON ${sql.id(table_name)}
-            FOR EACH ROW
-            EXECUTE FUNCTION update_updated_at();
-            `.execute(db)
-    }
+             BEFORE UPDATE ON ${sql.id(table)}
+             FOR EACH ROW
+             EXECUTE FUNCTION update_updated_at();
+    `.execute(db)
+  }
 }
 
 export async function down(db: Kysely<unknown>) {
-  const tables = await sql<{table_name: string}>
-  `SELECT table_name
-  FROM information_schema.tables
-  WHERE table_schema = 'public'
-    AND table_type = 'BASE TABLE'`
-    .execute(db)
-  for (const {table_name} of tables.rows){
-    await sql
-    `DROP TRIGGER IF EXIST update_updated_at_trigger
-      ON ${sql.id(table_name)}`.execute(db)
+  const tables = await selectAllNonKyselyMetaTables(db)
+  for (const table of tables){
+    await sql`
+      DROP TRIGGER IF EXIST update_updated_at_trigger
+        ON ${sql.id(table)}
+    `.execute(db)
   }
-
 }
