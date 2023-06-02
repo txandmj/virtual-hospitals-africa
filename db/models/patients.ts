4845e1ed3c99581387c4e389a91669a3ffb77f0e
Chun <peterpang1103@gmail.com>
2023-06-02 16:50:40 -0700

    fix ts compiler error

--- db/models/patients.ts
+++ db/models/patients.ts
@@ -23,7 +23,7 @@ export async function getByPhoneNumber(
 
 export async function upsert(trx: TrxOrDb, info: {
   id?: number
-  conversation_state: Maybe<ConversationState>
+  conversation_state: ConversationState
   phone_number: string
   name: Maybe<string>
   gender: Maybe<Gender>
