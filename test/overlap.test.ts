eca90de714a4c223bfde6982261830a897f6a91c
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-20 14:40:13 +0100

    Refactored test cases and removed redundant comment

--- test/overlap.test.ts
+++ test/overlap.test.ts
@@ -1,109 +1,74 @@
 import { assertEquals } from 'std/testing/asserts.ts'
-import { Time, TimeWindow } from '../types.ts'
+import { TimeWindow } from '../types.ts'
 import { windowsOverlap, overlaps } from '../islands/set-availability-form.tsx'
 
-function newTimeWindow(startTime: Time, endTime: Time): TimeWindow {
-  const timeWindow: TimeWindow = {
-    start: startTime,
-    end: endTime,
-  }
-  return timeWindow
-}
-
-//no overlap : single time slot
 Deno.test('windowsOverlap should return false for non-overlapping time slots', () => {
-  const input: TimeWindow[] = new Array<TimeWindow>()
-  input.push(
-    newTimeWindow({ hour: 9, minute: 0, amPm: 'am' }, {
-      hour: 5,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
+  const input: TimeWindow[] = [
+    {
+      start: { hour: 9, minute: 0, amPm: 'am' },
+      end: { hour: 5, minute: 0, amPm: 'pm' },
+    }
+  ];
   assertEquals(windowsOverlap(input), false)
 })
 
-//overlap:partially pattern 1 (9am-5pm and 4pm-6pm)
-Deno.test('windowsOverlap should return true for partially overlapping time slots', () => {
-  const input: TimeWindow[] = new Array<TimeWindow>()
-  input.push(
-    newTimeWindow({ hour: 9, minute: 0, amPm: 'am' }, {
-      hour: 5,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
-  input.push(
-    newTimeWindow({ hour: 4, minute: 0, amPm: 'pm' }, {
-      hour: 6,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
+Deno.test('windowsOverlap should return true for partially overlapping time slots: 9am-5pm and 4pm-6pm', () => {
+  const input: TimeWindow[] = [
+    {
+      start: { hour: 9, minute: 0, amPm: 'am' },
+      end: { hour: 5, minute: 0, amPm: 'pm' },
+    },
+    {
+      start: { hour: 4, minute: 0, amPm: 'pm' },
+      end: { hour: 6, minute: 0, amPm: 'pm' },
+    }
+  ];
   assertEquals(windowsOverlap(input), true)
 })
 
-//overlap:partially pattern 2 (3pm-6pm and 10am-5pm)
-Deno.test('windowsOverlap should return true for partially overlapping time slots', () => {
-  const input: TimeWindow[] = new Array<TimeWindow>()
-  input.push(
-    newTimeWindow({ hour: 3, minute: 0, amPm: 'pm' }, {
-      hour: 6,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
-  input.push(
-    newTimeWindow({ hour: 10, minute: 0, amPm: 'am' }, {
-      hour: 5,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
+Deno.test('windowsOverlap should return true for partially overlapping time slots: 3pm-6pm and 10am-5pm', () => {
+  const input: TimeWindow[] = [
+    {
+      start: { hour: 3, minute: 0, amPm: 'pm' },
+      end: { hour: 6, minute: 0, amPm: 'pm' },
+    },
+    {
+      start: { hour: 10, minute: 0, amPm: 'am' },
+      end: { hour: 5, minute: 0, amPm: 'pm' },
+    }
+  ];
   assertEquals(windowsOverlap(input), true)
 })
 
-//overlap : partially pattern 3 (10:30am-3:15pm and 11:30am-2:20pm)
-Deno.test('windowsOverlap should return true for partially overlapping time slots', () => {
-  const input: TimeWindow[] = new Array<TimeWindow>()
-  input.push(
-    newTimeWindow({ hour: 10, minute: 30, amPm: 'am' }, {
-      hour: 3,
-      minute: 15,
-      amPm: 'pm',
-    }),
-  )
-  input.push(
-    newTimeWindow({ hour: 11, minute: 30, amPm: 'am' }, {
-      hour: 2,
-      minute: 20,
-      amPm: 'pm',
-    }),
-  )
+Deno.test('windowsOverlap should return true for partially overlapping time slots: 10:30am-3:15pm and 11:30am-2:20pm', () => {
+  const input: TimeWindow[] = [
+    {
+      start: { hour: 10, minute: 30, amPm: 'am' },
+      end: { hour: 3, minute: 15, amPm: 'pm' },
+    },
+    {
+      start: { hour: 11, minute: 30, amPm: 'am' },
+      end: { hour: 2, minute: 20, amPm: 'pm' },
+    }
+  ];
   assertEquals(windowsOverlap(input), true)
 })
 
-//ovelap : completely same time
-Deno.test('windowsOverlap should return true for overlapping time slots', () => {
-  const input: TimeWindow[] = new Array<TimeWindow>()
-  input.push(
-    newTimeWindow({ hour: 3, minute: 0, amPm: 'pm' }, {
-      hour: 6,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
-  input.push(
-    newTimeWindow({ hour: 3, minute: 0, amPm: 'pm' }, {
-      hour: 6,
-      minute: 0,
-      amPm: 'pm',
-    }),
-  )
+Deno.test('windowsOverlap returns true when the first window is identical to the second window', () => {
+  const input: TimeWindow[] = [
+    {
+      start: { hour: 3, minute: 0, amPm: 'pm' },
+      end: { hour: 6, minute: 0, amPm: 'pm' },
+    },
+    {
+      start: { hour: 3, minute: 0, amPm: 'pm' },
+      end: { hour: 6, minute: 0, amPm: 'pm' },
+    }
+  ];
   assertEquals(windowsOverlap(input), true)
 })
 
-Deno.test('overlaps returns true when the first window includes the othjer', () => {
+Deno.test('overlaps returns true when the first window includes the other one', () => {
   assertEquals(
     overlaps(
       {
@@ -151,7 +116,7 @@ Deno.test("overlaps returns false even if the windows don't overlap, even if the
   )
 })
 
-Deno.test("overlaps returns false if two time slots do not overlap", () => {
+Deno.test("overlaps returns false if two windows do not overlap", () => {
   assertEquals(
     overlaps(
       {
