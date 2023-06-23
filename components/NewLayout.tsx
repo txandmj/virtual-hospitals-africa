eeb7625f375d3a164729bbddc787b57d2313af00
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-23 14:12:47 +0100

    Added home page, new layout and header

---
+++ components/NewLayout.tsx
@@ -0,0 +1,36 @@
+import { Head } from '$fresh/runtime.ts'
+import { JSX } from 'preact'
+import BottomNav from './BottomNav.tsx'
+import { Header } from './Header.tsx'
+
+export type LayoutProps = {
+  title: string
+  route: string
+  children: JSX.Element | JSX.Element[]
+  imageUrl?: string
+  isShowNav?: boolean
+}
+
+export default function NewLayout(props: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <link rel='stylesheet' href='/normalize.css' />
+        <link rel='stylesheet' href='/main.css' />
+        <script
+          defer
+          src='https://cdnjs.cloudflare.com/ajax/libs/turbolinks/5.2.0/turbolinks.js'
+          integrity='sha512-G3jAqT2eM4MMkLMyQR5YBhvN5/Da3IG6kqgYqU9zlIH4+2a+GuMdLb5Kpxy6ItMdCfgaKlo2XFhI0dHtMJjoRw=='
+          crossOrigin='anonymous'
+          referrerpolicy='no-referrer'
+        />
+      </Head>
+      <body className='h-full relative'>
+        <Header title={props.title} imageUrl={props.imageUrl} isShowNav={props.isShowNav} />
+        {props.children}
+        <BottomNav route={props.route} />
+      </body>
+    </>
+  )
+}
