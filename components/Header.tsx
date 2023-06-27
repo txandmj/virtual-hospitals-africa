5b49d84625fa42eba007c08aa4834c427ae627a2
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-06-27 16:46:17 +0100

    Added bell and arrow left icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import BellIcon from './icons/bell.tsx'
+import ArrowLeftIcon from './icons/arrow-left.tsx'
 export type HeaderProps = {
   title: string
   imageUrl?: string
@@ -15,16 +17,11 @@ export function Header({ title, imageUrl, isShowNav = true }: HeaderProps) {
                 className='back'
                 onClick={() => window.history.back()}
               >
-                <svg
-                  className='back-arrow w-4 h-4'
-                  viewBox='0 0 16 16'
-                >
-                  <path
-                    d='M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z'
-                    fill='white'
-                  >
-                  </path>
-                </svg>
+                <ArrowLeftIcon
+                  className='back-arrow w-4 h-4 fill-white'
+                  stroke-width='1'
+                  stroke='currentColor'
+                />
               </a>
             )}
             <h6 className='text-xl'>{title}</h6>
@@ -35,20 +32,12 @@ export function Header({ title, imageUrl, isShowNav = true }: HeaderProps) {
               className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
             >
               <span className='sr-only'>View notifications</span>
-              <svg
+              <BellIcon
                 className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
                 stroke-width='1.5'
                 stroke='currentColor'
                 aria-hidden='true'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
-                />
-              </svg>
+              />
             </button>
             {imageUrl && (
               <button
