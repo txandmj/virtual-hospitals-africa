35fc60799f3193583220ccfb94d6f201bcf1346a
Will Weiss <will.weiss1230@classdojo.com>
2023-06-26 20:34:13 -0400

    bump imports

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -10,12 +10,13 @@ import * as $4 from './routes/app/_middleware.tsx'
 import * as $5 from './routes/app/calendar.tsx'
 import * as $6 from './routes/app/calendar/appointments/[id].tsx'
 import * as $7 from './routes/app/calendar/set-availability.tsx'
-import * as $8 from './routes/chatbot/incoming-whatsapp.ts'
-import * as $9 from './routes/full-reset.ts'
-import * as $10 from './routes/index.tsx'
-import * as $11 from './routes/logged-in.tsx'
-import * as $12 from './routes/logout.tsx'
-import * as $13 from './routes/patient-reset.ts'
+import * as $8 from './routes/app/employees.tsx'
+import * as $9 from './routes/chatbot/incoming-whatsapp.ts'
+import * as $10 from './routes/full-reset.ts'
+import * as $11 from './routes/index.tsx'
+import * as $12 from './routes/logged-in.tsx'
+import * as $13 from './routes/logout.tsx'
+import * as $14 from './routes/patient-reset.ts'
 import * as $$0 from './islands/Menu.tsx'
 import * as $$1 from './islands/set-availability-form.tsx'
 
@@ -29,12 +30,13 @@ const manifest = {
     './routes/app/calendar.tsx': $5,
     './routes/app/calendar/appointments/[id].tsx': $6,
     './routes/app/calendar/set-availability.tsx': $7,
-    './routes/chatbot/incoming-whatsapp.ts': $8,
-    './routes/full-reset.ts': $9,
-    './routes/index.tsx': $10,
-    './routes/logged-in.tsx': $11,
-    './routes/logout.tsx': $12,
-    './routes/patient-reset.ts': $13,
+    './routes/app/employees.tsx': $8,
+    './routes/chatbot/incoming-whatsapp.ts': $9,
+    './routes/full-reset.ts': $10,
+    './routes/index.tsx': $11,
+    './routes/logged-in.tsx': $12,
+    './routes/logout.tsx': $13,
+    './routes/patient-reset.ts': $14,
   },
   islands: {
     './islands/Menu.tsx': $$0,
