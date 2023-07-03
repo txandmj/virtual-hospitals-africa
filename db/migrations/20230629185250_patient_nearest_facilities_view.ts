7eb4ed607806c6ab0bafb5051b1afa42913cb38a
Will Weiss <will.weiss1230@classdojo.com>
2023-07-03 13:06:56 -0400

    uses patients.location for patient_nearest_facilities view

--- db/migrations/20230629185250_patient_nearest_facilities_view.ts
+++ db/migrations/20230629185250_patient_nearest_facilities_view.ts
@@ -2,37 +2,38 @@ import { Kysely, sql } from 'kysely'
 
 export async function up(db: Kysely<unknown>) {
   await sql`
-    CREATE VIEW patient_nearest_facilities AS
+    CREATE VIEW patient_nearest_facilities AS (
+      WITH patients_with_location AS (
+            SELECT id as patient_id, location
+              FROM patients 
+             WHERE location is not null
+          ),
+
+      patient_facility_results AS (
+            SELECT facilities.*, 
+                   patients_with_location.patient_id as patient_id, 
+                   ST_Distance(patients_with_location.location, facilities.location) as distance, 
+                   ROW_NUMBER() OVER (
+                    PARTITION BY patients_with_location.patient_id 
+                    ORDER BY ST_Distance(patients_with_location.location, facilities.location)
+                   ) as row_number 
+              FROM patients_with_location
+        CROSS JOIN facilities
+      )
 
         SELECT patient_id,
-              json_agg(json_build_object(
+               json_agg(json_build_object(
                   'id', id,
                   'name', name,
                   'address', address,
                   'longitude', ST_X(location::geometry),
                   'latitude', ST_Y(location::geometry),
                   'distance', distance
-              )) as nearest_facilities
-        FROM (SELECT facilities.*, location.patient_id as patient_id,
-                    ST_Distance(
-                        location.location,
-                        facilities.location
-                    ) as distance,
-                    ROW_NUMBER() OVER (PARTITION BY location.patient_id ORDER BY ST_Distance(location.location, facilities.location)) as row_number
-              FROM (SELECT  DISTINCT ON (patient_id)
-                            patient_id,
-                            ST_MakePoint(
-                                        CAST(body::json ->> 'longitude' AS double precision),
-                                        CAST(body::json ->> 'latitude' AS double precision)
-                            )::geography AS location
-                      FROM whatsapp_messages_received
-                      WHERE conversation_state = 'find_nearest_facility:share_location'
-                      ORDER BY patient_id, updated_at DESC
-                    ) AS location
-              CROSS JOIN facilities
-              ) AS subq
-        WHERE subq.row_number <= 10
-        GROUP BY patient_id
+                )) as nearest_facilities
+          FROM patient_facility_results
+         WHERE patient_facility_results.row_number <= 10
+      GROUP BY patient_id
+    )
   `.execute(db)
 }
 
