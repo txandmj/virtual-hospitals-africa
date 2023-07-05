a7c0f30c7e96b7b178eb151685cc7bb8570876f5
jakekumra <jprkumra@gmail.com>
2023-07-05 13:50:11 -0600

    dummy migration data added

--- db/migrations/20230629120430_add_media.ts
+++ db/migrations/20230629120430_add_media.ts
@@ -1,9 +1,9 @@
 import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>) {
-  // await db.schema.createTable('test_add_media')
+  await db.schema.createTable('test_add_media')
 }
 
 export async function down(db: Kysely<unknown>) {
-  // await db.schema.dropTable('test_add_media')
+  await db.schema.dropTable('test_add_media')
 }
