1db58551038e9a05d8d86e5eeffa7c42c3b43963
Will Weiss <will.weiss1230@classdojo.com>
2023-07-19 12:54:58 -0400

    add initial chatbot test

--- test/chatbot/patient.test.ts
+++ test/chatbot/patient.test.ts
@@ -1,2 +1,59 @@
-Deno.test('It has a main menu', async () => {
+// deno-lint-ignore-file no-explicit-any
+import db from '../../db/db.ts'
+import respond from '../../chatbot/respond.ts'
+import * as conversations from '../../db/models/conversations.ts'
+import { afterEach, beforeEach, describe, it } from 'std/testing/bdd.ts'
+import sinon from 'npm:sinon'
+import reset from '../../db/reset.ts'
+import * as patients from '../../db/models/patients.ts'
+import {
+  assert,
+  assertEquals,
+} from 'https://deno.land/std@0.190.0/testing/asserts.ts'
+
+describe('patient chatbot', () => {
+  beforeEach(reset)
+  afterEach(() => db.destroy())
+
+  it('It sends the main menu after the initial message', async () => {
+    await conversations.insertMessageReceived(db, {
+      patient_phone_number: '2369961017',
+      has_media: false,
+      body: 'body',
+      media_id: null,
+      whatsapp_id: 'whatsapp_id',
+    })
+
+    const fakeWhatsApp = {
+      sendMessage: sinon.stub().resolves({} as any),
+      sendMessages: sinon.stub().resolves([{
+        messages: [{
+          id: 'wamid.1234',
+        }],
+      }]),
+    }
+
+    await respond(fakeWhatsApp)
+    assertEquals(fakeWhatsApp.sendMessages.firstCall.args, [
+      {
+        messages: {
+          messageBody:
+            'Welcome to Virtual Hospitals Africa. What can I help you with today?',
+          type: 'buttons',
+          buttonText: 'Menu',
+          options: [
+            { id: 'make_appointment', title: 'Make Appointment' },
+            { id: 'find_nearest_facility', title: 'Nearest Facility' },
+          ],
+        },
+        phone_number: '2369961017',
+      },
+    ])
+    const patient = await patients.getByPhoneNumber(db, {
+      phone_number: '2369961017',
+    })
+
+    assert(patient)
+    assertEquals(patient.conversation_state, 'not_onboarded:welcome')
+  })
 })
