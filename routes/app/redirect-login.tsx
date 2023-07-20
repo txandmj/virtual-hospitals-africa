10420390bff2042ad38070fa728d63cea28bc525
natsumi <otan.tmd@gmail.com>
2023-07-20 08:04:34 +0100

    Work on redirect-login

--- routes/app/redirect-login.tsx
+++ routes/app/redirect-login.tsx
@@ -1,14 +1,21 @@
-import { useEffect, useRef } from 'preact/hooks'
+import { useEffect } from 'preact/hooks'
 import { oauthParams } from '../../external-clients/google.ts'
 import redirect from '../../util/redirect.ts'
 
+const loginUrl =
+  `https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?${oauthParams}`
+
+//work in progress: redirect currently does not work
 export default function Redirect() {
-  const redirectTimeout = setTimeout(() => {
-    console.log('get into use effect?')
-    const url = '/app'
-    //`https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?${oauthParams}`
-    redirect('/app')
-  }, 3000)
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      console.log('get into here?')
+      redirect(loginUrl)
+    }, 3000)
+    return () => {
+      clearTimeout(redirectTimeout)
+    }
+  }, [])
 
   return (
     <div className='rounded-md bg-yellow-50 p-4'>
@@ -20,8 +27,17 @@ export default function Redirect() {
           <div className='mt-2 text-sm text-yellow-700'>
             <p>
               If you are not redirected in 3 seconds, please{' '}
-              <button onClick={() => redirect('/app')}>
-                click here
+              <button
+                style={{ fontWeight: 'bold', textDecoration: 'none' }}
+                onMouseOver={(e) =>
+                  e.currentTarget.style.textDecoration = 'underline'}
+                onMouseOut={(e) =>
+                  e.currentTarget.style.textDecoration = 'none'}
+                onClick={() => {
+                  return Response.redirect(loginUrl, 307)
+                }}
+              >
+                click here.
               </button>
             </p>
           </div>
