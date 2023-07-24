443d3de8553469ca3e5e3c21ab662ffe15a63bd8
Will Weiss <will.weiss1230@classdojo.com>
2023-07-24 10:37:44 -0400

    update lock file

---
+++ test/chatbot/patient2.test.ts
@@ -0,0 +1,57 @@
+import { afterEach, beforeEach, describe, it } from 'std/testing/bdd.ts'
+import { assert, assertEquals } from 'std/testing/asserts.ts'
+import sinon from 'npm:sinon'
+import db from '../../db/db.ts'
+// import {redis} from '../../external-clients/redis.ts'
+import reset from '../../db/reset.ts'
+import * as conversations from '../../db/models/conversations.ts'
+import * as patients from '../../db/models/patients.ts'
+import respond from '../../chatbot/respond.ts'
+
+describe('patient chatbot', () => {
+  beforeEach(reset)
+  afterEach(() => db.destroy())
+  // afterEach(() => redis.close())
+
+  it('foo', async () => {
+    await conversations.insertMessageReceived(db, {
+      patient_phone_number: '2369961017',
+      has_media: false,
+      body: 'body',
+      media_id: null,
+      whatsapp_id: 'whatsapp_id',
+    })
+
+    const fakeWhatsApp = {
+      sendMessage: sinon.stub().throws(),
+      sendMessages: sinon.stub().resolves([{
+        messages: [{
+          id: 'wamid.1234',
+        }],
+      }]),
+    }
+
+    await respond(fakeWhatsApp)
+    assertEquals(fakeWhatsApp.sendMessages.firstCall.args, [
+      {
+        messages: {
+          messageBody:
+            'Welcome to Virtual Hospitals Africa. What can I help you with today?',
+          type: 'buttons',
+          buttonText: 'Menu',
+          options: [
+            { id: 'make_appointment', title: 'Make Appointment' },
+            { id: 'find_nearest_facility', title: 'Nearest Facility' },
+          ],
+        },
+        phone_number: '2369961017',
+      },
+    ])
+    const patient = await patients.getByPhoneNumber(db, {
+      phone_number: '2369961017',
+    })
+
+    assert(patient)
+    assertEquals(patient.conversation_state, 'not_onboarded:welcome')
+  })
+})
