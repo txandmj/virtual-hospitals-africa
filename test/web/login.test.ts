76aeae41aff6075ce323e3dff1d907ebdb8f6731
adastomek <adastomek123@gmail.com>
2023-08-16 22:39:11 +0100

    fixed broken pipe, db wipe then latest

--- test/web/login.test.ts
+++ test/web/login.test.ts
@@ -3,15 +3,33 @@ import { redis } from '../../external-clients/redis.ts'
 import db from '../../db/db.ts'
 import { assert } from 'https://deno.land/std@0.190.0/testing/asserts.ts'
 import { upsertWithGoogleCredentials } from '../../db/models/health_workers.ts'
-import reset from '../../db/reset.ts'
 import { readLines } from 'https://deno.land/std@0.140.0/io/buffer.ts'
 import { readerFromStreamReader } from 'https://deno.land/std@0.140.0/streams/conversion.ts'
+import * as employee from '../../db/models/employment.ts'
 
 describe('/login', () => {
   const PORT = '8002'
   const ROUTE = `https://localhost:${PORT}`
+
+  const testHealthWorker = {
+    name: 'Test Health Worker',
+    email: 'test@healthworker.com',
+    avatar_url:
+      'https://lh3.googleusercontent.com/a/AAcHTtdCli8DiIjBkdb9TZL3W46MoxFPOy2Xuqkm345WiS446Ow=s96-c',
+    gcal_appointments_calendar_id:
+      'vjf3q6onfgnn83me7rf10fdcj4@group.calendar.google.com',
+    gcal_availability_calendar_id:
+      'fq5vbod94ihhherp9fad2tlaqk@group.calendar.google.com',
+    access_token: 'ya29.whateverlrkwlkawlk-tl2O85WA2QW_1Lf_P4lRqyAG4aUCIo0D18F',
+    expires_in: 3599,
+    refresh_token:
+      '1//01_ao4e0Kf-uTCgYIARAAGAESNwF-L9IrQkmis6YBAP4NE7BWrI7ry1qSeotPA_DLMYW9yiGLUUsaOjy7rlUvYs2nL_BTFjuv',
+    expires_at: '2023-07-25T19:20:45.123Z',
+  }
+
   let process: Deno.ChildProcess
   beforeAll(async () => {
+    await dbWipeThenLatest()
     process = new Deno.Command('deno', {
       args: [
         'task',
@@ -40,9 +58,9 @@ describe('/login', () => {
       line = (await lineReader.next()).value
     } while (line !== `Listening on ${ROUTE}/`)
     stdout.releaseLock()
-    await process.stdout.cancel()
   })
-  afterAll(() => {
+  afterAll(async () => {
+    await process.stdout.cancel()
     process.kill()
   })
 
@@ -62,25 +80,7 @@ describe('/login', () => {
 
   describe('when logged in', () => {
     beforeAll(async () => {
-      await reset()
-      const testHealthWorker = {
-        name: 'Test Health Worker',
-        email: 'test@healthworker.com',
-        avatar_url:
-          'https://lh3.googleusercontent.com/a/AAcHTtdCli8DiIjBkdb9TZL3W46MoxFPOy2Xuqkm345WiS446Ow=s96-c',
-        gcal_appointments_calendar_id:
-          'vjf3q6onfgnn83me7rf10fdcj4@group.calendar.google.com',
-        gcal_availability_calendar_id:
-          'fq5vbod94ihhherp9fad2tlaqk@group.calendar.google.com',
-        access_token:
-          'ya29.whateverlrkwlkawlk-tl2O85WA2QW_1Lf_P4lRqyAG4aUCIo0D18F',
-        expires_in: 3599,
-        refresh_token:
-          '1//01_ao4e0Kf-uTCgYIARAAGAESNwF-L9IrQkmis6YBAP4NE7BWrI7ry1qSeotPA_DLMYW9yiGLUUsaOjy7rlUvYs2nL_BTFjuv',
-        expires_at: '2023-07-25T19:20:45.123Z',
-      }
       const upserted = await upsertWithGoogleCredentials(db, testHealthWorker)
-
       await redis.set(
         'S_123',
         JSON.stringify({
@@ -95,15 +95,66 @@ describe('/login', () => {
     it('redirects to /app', async () => {
       const response = await fetch(`${ROUTE}/login`, {
         redirect: 'manual',
-        credentials: 'same-origin',
         headers: {
           Cookie: 'sessionId=123',
         },
       })
-
       const redirectLocation = response.headers.get('location')
       assert(redirectLocation === '/app')
-      if (response.body) response.body.cancel()
+      response.body?.cancel()
+    })
+
+    it('unemployed is unauthorized', async () => {
+      const response = await fetch(`${ROUTE}/app`, {
+        headers: {
+          Cookie: 'sessionId=123',
+        },
+      })
+      assert(!response.ok)
+      response.body?.cancel()
+    })
+
+    it('unregistered nurse is redirected to registration', async () => {
+      await employee.add(db, [{
+        facility_id: 1,
+        health_worker_id: 1,
+        profession: 'nurse',
+      }])
+
+      await fetch(`${ROUTE}/app`, {
+        headers: {
+          Cookie: 'sessionId=123',
+        },
+      }).then((response) => {
+        assert(response.ok)
+        assert(
+          response.text().then((text) => {
+            text.includes('Personal')
+          }),
+        )
+      })
     })
   })
 })
+
+async function dbWipeThenLatest() {
+  await new Deno.Command('deno', {
+    args: [
+      'task',
+      'db:migrate:wipe',
+    ],
+    stdin: 'null',
+    stdout: 'null',
+    stderr: 'null',
+  }).output()
+
+  await new Deno.Command('deno', {
+    args: [
+      'task',
+      'db:migrate:latest',
+    ],
+    stdin: 'null',
+    stdout: 'null',
+    stderr: 'null',
+  }).output()
+}
