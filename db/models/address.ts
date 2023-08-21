c4abeaec15836d96d6a169ad0f30a932f4aa1d9c
Will Weiss <will.weiss1230@classdojo.com>
2023-08-21 18:46:46 -0400

    nested address info

---
+++ db/models/address.ts
@@ -0,0 +1,50 @@
+import { TrxOrDb } from '../../types.ts'
+import { jsonArrayFrom } from '../helpers.ts'
+
+export function getAll(
+  trx: TrxOrDb,
+) {
+  return trx
+    .selectFrom('countries')
+    .select((ebProvinces) => [
+      'id',
+      'name',
+      jsonArrayFrom(
+        ebProvinces.selectFrom('provinces')
+          .select((ebDistricts) => [
+            'id',
+            'name',
+            jsonArrayFrom(
+              ebDistricts.selectFrom('districts')
+                .select((ebWards) => [
+                  'id',
+                  'name',
+                  jsonArrayFrom(
+                    ebWards.selectFrom('wards')
+                      .select((ebSuburbs) => [
+                        'id',
+                        'name',
+                        jsonArrayFrom(
+                          ebSuburbs.selectFrom('suburbs')
+                            .select([
+                              'id',
+                              'name',
+                            ])
+                            .whereRef(
+                              'suburbs.ward_id',
+                              '=',
+                              'wards.id',
+                            ),
+                        ).as('suburbs'),
+                      ])
+                      .whereRef('wards.district_id', '=', 'districts.id'),
+                  ).as('wards'),
+                ])
+                .whereRef('districts.province_id', '=', 'provinces.id'),
+            ).as('districts'),
+          ])
+          .whereRef('provinces.country_id', '=', 'countries.id'),
+      ).as('provinces'),
+    ])
+    .execute()
+}
