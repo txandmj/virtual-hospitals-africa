4b7830621878828b16c917875106f99d94d54540
Chun <peterpang1103@gmail.com>
2023-09-12 22:48:46 -0700

    adding support for video and audio

--- components/library/Media.tsx
+++ components/library/Media.tsx
@@ -2,13 +2,16 @@ export default function Media(
   { src, mime_type }: { src: string; mime_type: string },
 ) {
   const media_type = mime_type.toLowerCase()
-  const element = media_type.startsWith('image/')
-    ? (
-      <img
-        src={src}
-        alt='alt'
-      />
+  if (media_type.startsWith('image/')) {
+    return <img src={src} />
+  } else if (media_type.startsWith('video/')) {
+    return <video controls src={src} />
+  } else if (media_type.startsWith('audio/')) {
+    return (
+      <audio controls src={src}>
+      </audio>
     )
-    : <video src={src}></video>
-  return element
+  }
+
+  return <p>Unspported Media Type: {mime_type}</p>
 }
