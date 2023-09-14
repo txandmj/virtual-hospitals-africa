ea73a72fded7cba3aa9c4f77f2e0b6626272c541
debruler <debrulerij@vcu.edu>
2023-09-14 12:37:27 -0400

    form scaffolding

--- components/patients/add/FamilyForm.tsx
+++ components/patients/add/FamilyForm.tsx
@@ -1,7 +1,28 @@
+import { SelectInput, TextInput } from "../../library/form/Inputs.tsx"
+import FormRow from "../../library/form/Row.tsx"
+
 export default function FamilyForm() {
     return (
-        <div>
-            test
-        </div>
+        <>
+            <FormRow>
+                <SelectInput name='marital status' required label='Marial Status'>
+                    <option value='single'>Single</option>
+                    <option value='married'>Married</option>
+                    <option value='civilPartner'>Civil Partner</option>
+                    <option value='widowWidower'>Widow/Widower</option>
+                    <option value='separated'>Separated</option>
+                    <option value='divorced'>Divorced</option>
+                </SelectInput>
+                <SelectInput name='religion' required label="Religion">
+                    
+                </SelectInput>
+            </FormRow>
+            <h1>Next of Kin</h1>
+            <FormRow>
+                <TextInput name='name'/>
+                <TextInput name='phone number'/>
+                <TextInput name='relationship' />
+            </FormRow>
+        </>
     )
 }
