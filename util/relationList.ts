085d855968159ecd0326cbdab011ab919f658185
ExplanationMark98 <johnduffer1@gmail.com>
2023-09-14 14:00:38 -0400

    Made relationList.ts & religionList.ts

---
+++ util/relationList.ts
@@ -0,0 +1,14 @@
+export const allRelations = [
+     'Wife',
+ 'Husband',
+ 'Brother',
+ 'Sister',
+ 'Grandparent',
+ 'Grandchild',
+ 'Son',
+ 'Daughter',
+ 'Uncle',
+ 'Aunt',
+ 'Cousin',
+ 'Other Relative or Friend',
+  ]
