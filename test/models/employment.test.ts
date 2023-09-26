2ecabb5ff0d10ee60ad9bb82f88e41912ee20bd3
Rishab Sanyal <rsanyal@ucdavis.edu>
2023-09-25 18:07:45 -0700

    Adding test changes

--- test/models/employment.test.ts
+++ test/models/employment.test.ts
@@ -21,81 +21,84 @@ describe('db/models/employment.ts', () => {
     // Make the third health worker a doctor - Done
 
     const prev_worker_1 = await health_workers.upsert(db, {
-        name: 'Previous Worker 1',
-        email: 'previous1@worker.com',
-        avatar_url: 'avatar_url',
-        gcal_appointments_calendar_id: 'gcal_appointments_calendar_id',
-        gcal_availability_calendar_id: 'gcal_availability_calendar_id',
+      name: 'Previous Worker 1',
+      email: 'previous1@worker.com',
+      avatar_url: 'avatar_url',
+      gcal_appointments_calendar_id: 'gcal_appointments_calendar_id',
+      gcal_availability_calendar_id: 'gcal_availability_calendar_id',
     })
     const prev_worker_1_id = prev_worker_1.id
     assert(prev_worker_1)
 
     const prev_worker_2 = await health_workers.upsert(db, {
-        name: 'Previous Worker 2',
-        email: 'previous2@worker.com',
-        avatar_url: 'avatar_url',
-        gcal_appointments_calendar_id: 'gcal_appointments_calendar_id',
-        gcal_availability_calendar_id: 'gcal_availability_calendar_id',
+      name: 'Previous Worker 2',
+      email: 'previous2@worker.com',
+      avatar_url: 'avatar_url',
+      gcal_appointments_calendar_id: 'gcal_appointments_calendar_id',
+      gcal_availability_calendar_id: 'gcal_availability_calendar_id',
     })
     const prev_worker_2_id = prev_worker_2.id
     assert(prev_worker_2)
 
     const prev_worker_3 = await health_workers.upsert(db, {
-        name: 'Previous Worker 3',
-        email: 'previous3@worker.com',
-        avatar_url: 'avatar_url',
-        gcal_appointments_calendar_id: 'gcal_appointments_calendar_id',
-        gcal_availability_calendar_id: 'gcal_availability_calendar_id',
+      name: 'Previous Worker 3',
+      email: 'previous3@worker.com',
+      avatar_url: 'avatar_url',
+      gcal_appointments_calendar_id: 'gcal_appointments_calendar_id',
+      gcal_availability_calendar_id: 'gcal_availability_calendar_id',
     })
     assert(prev_worker_3)
 
     await employment.add(db, [
-        {
-            health_worker_id: prev_worker_1.id,
-            facility_id: 3,
-            profession: 'doctor',
-        },
-        {
-            health_worker_id: prev_worker_2.id,
-            facility_id: 3,
-            profession: 'doctor',
-        },
-        {
-            health_worker_id: prev_worker_1.id,
-            facility_id: 3,
-            profession: 'admin',
-        },
-        {
-            health_worker_id: prev_worker_2.id,
-            facility_id: 3,
-            profession: 'nurse',
-        },
-        {
-            health_worker_id: prev_worker_3.id,
-            facility_id: 4,
-            profession: 'doctor',
-        }
+      {
+        health_worker_id: prev_worker_1.id,
+        facility_id: 3,
+        profession: 'doctor',
+      },
+      {
+        health_worker_id: prev_worker_2.id,
+        facility_id: 3,
+        profession: 'doctor',
+      },
+      {
+        health_worker_id: prev_worker_1.id,
+        facility_id: 3,
+        profession: 'admin',
+      },
+      {
+        health_worker_id: prev_worker_2.id,
+        facility_id: 3,
+        profession: 'nurse',
+      },
+      {
+        health_worker_id: prev_worker_3.id,
+        facility_id: 4,
+        profession: 'doctor',
+      },
     ])
 
-    const professions_info:  Map<number, string> = new Map<number, string>([
-            [prev_worker_1_id, [ 'admin', 'doctor' ].join(', ')],
-            [prev_worker_2_id, [ 'doctor', 'nurse' ].join(', ')]
-    ]);
+    const professions_info: Map<number, string> = new Map<number, string>([
+      [prev_worker_1_id, ['admin', 'doctor'].join(', ')],
+      [prev_worker_2_id, ['doctor', 'nurse'].join(', ')],
+    ])
 
     const facility_id = 3
-    const result =
-        await employment.getEmployeeAndInviteeByFacility(db, {facility_id})
+    const result = await employment.getEmployeeAndInviteeByFacility(db, {
+      facility_id,
+    })
 
     assert(result, 'Result doesn\'t exist')
     assertEquals(result.length, 2, result.toLocaleString())
 
-    for (const resultValue of result){
-        const expected_professions = professions_info.get(resultValue.health_worker_id)
+    for (const resultValue of result) {
+      const expected_professions = professions_info.get(
+        resultValue.health_worker_id,
+      )
 
-        assertEquals(
-            expected_professions,
-            resultValue.professions
-        )
+      assertEquals(
+        expected_professions,
+        resultValue.professions,
+      )
     }
   })
 })
