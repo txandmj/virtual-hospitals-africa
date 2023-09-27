47b9a68cead171a2a22f4b828e30be7b38acc627
Will Weiss <will.weiss1230@classdojo.com>
2023-09-27 04:05:52 -0400

    faster reset

--- db/reset.ts
+++ db/reset.ts
@@ -12,12 +12,19 @@ export default async function reset(trx: TrxOrDb = db) {
   }
 }
 
+// facilities is also top level, but we don't want to delete it
+// as it is seeded by the migrations
+const topLevelTables = [
+  'patients' as const,
+  'health_workers' as const,
+  'health_worker_invitees' as const,
+  'media' as const,
+]
+
 export async function resetInTest(trx: TrxOrDb = db) {
   assert(Deno.env.get('IS_TEST'), 'Don\'t run this outside tests!')
-  await Promise.all([
-    trx.deleteFrom('patients').execute(),
-    trx.deleteFrom('health_workers').execute(),
-    trx.deleteFrom('health_worker_invitees').execute(),
-    trx.deleteFrom('media').execute(),
-  ])
+
+  await Promise.all(
+    topLevelTables.map((table) => trx.deleteFrom(table).execute()),
+  )
 }
