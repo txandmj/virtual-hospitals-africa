c412a37d437fe43e0c476eb0b10ee738563db3a0
Will Weiss <will.weiss1230@gmail.com>
2023-10-10 15:22:48 -0400

    aborting for now

--- heroku-run.sh
+++ heroku-run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
 
 ARTIFACT_NAME="$1"
 
@@ -13,37 +13,40 @@ if [ "$ARTIFACT_NAME" = "web" ]; then
   deno task db:migrate:latest
 fi
 
-get_github() {
-  curl -L \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_READ_TOKEN" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "$1"
-}
+deno task $ARTIFACT_NAME
 
-get_artifacts() {
-  get_github https://api.github.com/repos/morehumaninternet/virtual-hospitals-africa/actions/artifacts
-}
+# TODO: run from github artifacts if available
+# get_github() {
+#   curl -L \
+#     -H "Accept: application/vnd.github+json" \
+#     -H "Authorization: Bearer $GITHUB_READ_TOKEN" \
+#     -H "X-GitHub-Api-Version: 2022-11-28" \
+#     "$1"
+# }
 
-make_deno_script() {
-  printf "const archive_download_url = "
-  get_artifacts
-  echo ".artifacts.find(a => a.name == '$ARTIFACT_NAME' && a.workflow_run.head_sha == '$HEROKU_SLUG_COMMIT')?.archive_download_url;"
-  echo "if (archive_download_url) console.log(archive_download_url);"
-}
+# get_artifacts() {
+#   get_github https://api.github.com/repos/morehumaninternet/virtual-hospitals-africa/actions/artifacts
+# }
 
-SCRIPT=$(mktemp)
-make_deno_script > $SCRIPT
-ARTIFACT_URL=$(deno run $SCRIPT)
+# make_deno_script() {
+#   printf "const archive_download_url = "
+#   get_artifacts
+#   echo ".artifacts.find(a => a.name == '$ARTIFACT_NAME' && a.workflow_run.head_sha == '$HEROKU_SLUG_COMMIT')?.archive_download_url;"
+#   echo "if (archive_download_url) console.log(archive_download_url);"
+# }
 
-if [ -z "$ARTIFACT_URL" ]; then
-  echo "No matching artifact found, running $ARTIFACT_NAME from source"
-  deno task $ARTIFACT_NAME
-else
-  echo "Found artifact for $ARTIFACT_NAME, downloading and running binary"
-  get_github $ARTIFACT_URL > $ARTIFACT_NAME.zip
-  unzip $ARTIFACT_NAME.zip
-  rm $ARTIFACT_NAME.zip
-  chmod +x $ARTIFACT_NAME
-  ./$ARTIFACT_NAME
-fi
+# SCRIPT=$(mktemp)
+# make_deno_script > $SCRIPT
+# ARTIFACT_URL=$(deno run $SCRIPT)
+
+# if [ -z "$ARTIFACT_URL" ]; then
+#   echo "No matching artifact found, running $ARTIFACT_NAME from source"
+#   deno task $ARTIFACT_NAME
+# else
+#   echo "Found artifact for $ARTIFACT_NAME, downloading and running binary"
+#   get_github $ARTIFACT_URL > $ARTIFACT_NAME.zip
+#   unzip $ARTIFACT_NAME.zip
+#   rm $ARTIFACT_NAME.zip
+#   chmod +x $ARTIFACT_NAME
+#   ./$ARTIFACT_NAME
+# fi
