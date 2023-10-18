e7af19be7f9e2a76eb2e2e3becce94450996af83
Chun <peterpang1103@gmail.com>
2023-10-17 22:42:38 -0700

    change test data to date object

--- test/date.test.ts
+++ test/date.test.ts
@@ -4,7 +4,7 @@ import { prettyPatientDateOfBirth } from '../util/date.ts'
 
 Deno.test('prettyPatientDateOfBirth', () => {
   const dob = prettyPatientDateOfBirth({
-    date_of_birth: '1990-03-01',
+    date_of_birth: new Date('1990-03-01'),
   } as PatientDemographicInfo)
   assertEquals(dob, 'March 1, 1990')
 })
