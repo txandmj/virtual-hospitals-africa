f16f6ca9593436f8cbce2af50d00d79f037f222b
Will Weiss <will.weiss1230@gmail.com>
2023-10-20 10:34:02 -0400

    beginnings to add apply page

--- routes/apply.tsx
+++ routes/apply.tsx
@@ -1,13 +1,11 @@
-import {
-  EmployedHealthWorker,
-  LoggedInHealthWorkerHandler,
-} from '../../types.ts'
+import { EmployedHealthWorker, LoggedInHealthWorkerHandler } from '../types.ts'
 import { assert } from 'std/assert/assert.ts'
 import { PageProps } from '$fresh/server.ts'
-import Layout from '../../components/library/Layout.tsx'
-import { FacilityAdmin, getFacilityAdmin } from '../../db/models/employment.ts'
-import { Button } from '../../components/library/Button.tsx'
-import PageHeader from '../../components/library/typography/PageHeader.tsx'
+import Layout from '../components/library/Layout.tsx'
+import { FacilityAdmin, getFacilityAdmin } from '../db/models/employment.ts'
+import { Button } from '../components/library/Button.tsx'
+import PageHeader from '../components/library/typography/PageHeader.tsx'
+import { json } from '../util/responses.ts'
 
 type PendingApprovalPageProps = {
   healthWorker: EmployedHealthWorker
@@ -15,29 +13,15 @@ type PendingApprovalPageProps = {
 }
 
 export const handler: LoggedInHealthWorkerHandler<PendingApprovalPageProps> = {
-  async GET(_req, ctx) {
-    const { healthWorker } = ctx.state
-
-    const facilityAdmin = await getFacilityAdmin(ctx.state.trx, {
-      facility_id: healthWorker.employment[0].facility_id,
-    })
-
-    assert(facilityAdmin)
-
-    return ctx.render({
-      healthWorker,
-      facilityAdmin,
-    })
+  async POST(_req, ctx) {
+    // TODO
+    return json({ message: 'ok' })
   },
 }
 
-export default function PendingApprovalPage(
+export default function ApplyPage(
   props: PageProps<PendingApprovalPageProps>,
 ) {
-  const { facilityAdmin } = props.data
-  const facilityName = facilityAdmin.facility_name || 'your facility'
-  const facilityAdminName = facilityAdmin.name || 'your facility admin'
-
   return (
     <Layout
       title='Virtual Hospitals Africa'
@@ -50,9 +34,9 @@ export default function PendingApprovalPage(
             <div class='lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8'>
               <PageHeader className='h1'>Application under review</PageHeader>
               <p class='mt-6 text-xl leading-8 text-gray-600'>
-                Your application from {facilityName}{' '}
-                is currently under review by{' '}
-                {facilityAdminName}. You will receive an email once your
+                Your application from facilityName is currently under review by
+                {' '}
+                facilityAdminName. You will receive an email once your
                 application has been approved.
               </p>
               <div class='mt-10 flex'>
