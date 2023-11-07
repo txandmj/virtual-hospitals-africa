f6316c451392c28bdca8e3fbd25e720a8ea84f6d
Ashley Radford <ashleyradford23@gmail.com>
2023-11-06 20:25:09 -0800

    Add address table migration

---
+++ db/migrations/20231106235733_add_address_table.ts
@@ -0,0 +1,64 @@
+import { Kysely } from "kysely";
+
+export async function up(db: Kysely<unknown>) {
+  await db.schema
+    .createTable('address')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('street', 'varchar(255)')
+    .addColumn('suburb_id', 'integer', (col) =>
+      col.references('suburbs.id'))
+    .addColumn('ward_id', 'integer', (col) =>
+      col.notNull()
+        .references('wards.id'))
+    .addColumn('district_id', 'integer', (col) =>
+      col.notNull()
+        .references('districts.id'))
+    .addColumn('province_id', 'integer', (col) =>
+      col.notNull()
+        .references('provinces.id'))
+    .addColumn('country_id', 'integer', (col) =>
+      col.notNull()
+        .references('countries.id'))
+    .addUniqueConstraint('address_street_suburb_ward', [
+      'street',
+      'suburb_id',
+      'ward_id',
+    ])
+    .execute()
+
+  await db.schema
+    .alterTable('patients')
+    .dropColumn('street')
+    .dropColumn('suburb_id')
+    .dropColumn('ward_id')
+    .dropColumn('district_id')
+    .dropColumn('province_id')
+    .dropColumn('country_id')
+    .addColumn('address_id', 'integer', (col) =>
+      col.notNull()
+        .references('address.id'))
+    .execute()
+}
+
+export async function down(db: Kysely<unknown>) {
+  await db.schema.dropTable('address').execute()
+  await db.schema
+    .alterTable('patients')
+    .dropColumn('address_id')
+    .addColumn('street', 'varchar(255)')
+    .addColumn('suburb_id', 'integer', (col) =>
+      col.references('suburbs.id'))
+    .addColumn('ward_id', 'integer', (col) =>
+      col.notNull()
+        .references('wards.id'))
+    .addColumn('district_id', 'integer', (col) =>
+      col.notNull()
+        .references('districts.id'))
+    .addColumn('province_id', 'integer', (col) =>
+      col.notNull()
+        .references('provinces.id'))
+    .addColumn('country_id', 'integer', (col) =>
+      col.notNull()
+        .references('countries.id'))
+    .execute()
+}
