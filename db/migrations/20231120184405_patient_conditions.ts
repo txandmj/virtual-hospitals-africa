a1aaa1886cdf52bd52be4dabb1eed26e91f1b567
ExplanationMark98 <johnduffer1@gmail.com>
2023-11-28 16:13:34 -0500

    Patient Condition Table Creation

---
+++ db/migrations/20231120184405_patient_conditions.ts
@@ -0,0 +1,43 @@
+//deno-lint-ignore-file no-explicit-any
+import { Kysely, sql } from 'kysely'
+import { addUpdatedAtTrigger } from '../addUpdatedAtTrigger.ts'
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('patient_conditions')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn(
+      'created_at',
+      'timestamp',
+      (col) => col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn(
+      'updated_at',
+      'timestamp',
+      (col) => col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn('patient_id', 'integer', (col) =>
+      col
+        .notNull()
+        .references('patients.id')
+        .onDelete('cascade'))
+    .addColumn('condition_key_id', 'varchar(255)', (col) =>
+      col
+        .notNull()
+        .references('conditions.key_id')
+        .onDelete('cascade'))
+    .addColumn('start_date', 'date', (col) =>
+      col
+        .notNull())
+    .addColumn('end_date', 'date')
+    .addColumn('comorbidity_of_condition_id', 'integer', (col) =>
+      col
+        .references('patient_conditions.id')
+        .onDelete('cascade'))
+    .execute()
+  await addUpdatedAtTrigger(db, 'patient_conditions')
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('patient_conditions').execute()
+}
