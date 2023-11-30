90429f9663321887a7cd5200b85e7c851df587ca
ybsolomon <ybsolomon@dons.usfca.edu>
2023-11-29 22:06:55 -0800

    null address columns are considered unique noe

---
+++ db/migrations/20231130074151_make_null_address_columns_unique.ts
@@ -0,0 +1,29 @@
+import { Kysely, sql } from 'kysely'
+
+export async function up(db: Kysely<unknown>) {
+  await db.schema
+    .alterTable('address')
+    .dropConstraint('address_street_suburb_ward')
+    .execute()
+
+  await sql`
+  ALTER TABLE address
+  ADD CONSTRAINT address_street_suburb_ward UNIQUE NULLS NOT DISTINCT (street, suburb_id, ward_id)`
+    .execute(db)
+}
+
+export async function down(db: Kysely<unknown>) {
+  await db.schema
+    .alterTable('address')
+    .dropConstraint('address_street_suburb_ward')
+    .execute()
+
+  await db.schema
+    .alterTable('address')
+    .addUniqueConstraint('address_street_suburb_ward', [
+      'street',
+      'suburb_id',
+      'ward_id',
+    ])
+    .execute()
+}
