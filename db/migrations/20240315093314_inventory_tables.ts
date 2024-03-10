0ab1f89a29ab12fef819b66fa365a680c61c808b
Shervin <sherviniv@gmail.com>
2024-03-10 13:22:27 +0330

    Add consumables models migration

--- db/migrations/20240315093314_inventory_tables.ts
+++ db/migrations/20240315093314_inventory_tables.ts
@@ -1,64 +1,146 @@
-import { Kysely } from 'kysely'
+import { Kysely, sql } from 'kysely'
 import { createStandardTable } from '../createStandardTable.ts'
+import * as inParallel from '../../util/inParallel.ts'
 
 export async function up(db: Kysely<unknown>) {
-  await createStandardTable(
-    db,
-    'devices',
-    (qb) =>
-      qb.addColumn('name', 'varchar(255)', (col) => col.notNull())
-        .addColumn('manufacturer', 'varchar(255)', (col) => col.notNull()),
+  await createStandardTable(db, 'devices', (qb) =>
+    qb
+      .addColumn('name', 'varchar(255)', (col) => col.notNull())
+      .addColumn('manufacturer', 'varchar(255)', (col) => col.notNull())
   )
 
-  await createStandardTable(
-    db,
-    'device_capabilities',
-    (qb) =>
-      qb.addColumn(
-        'device_id',
-        'integer',
-        (col) => col.notNull().references('devices.id').onDelete('cascade'),
+  await createStandardTable(db, 'device_capabilities', (qb) =>
+    qb
+      .addColumn('device_id', 'integer', (col) =>
+        col.notNull().references('devices.id').onDelete('cascade')
       )
-        .addColumn(
-          'diagnostic_test',
-          'varchar(40)',
-          (col) =>
-            col.notNull().references('diagnostic_tests.name').onDelete(
-              'cascade',
-            ),
-        ),
+      .addColumn('diagnostic_test', 'varchar(40)', (col) =>
+        col.notNull().references('diagnostic_tests.name').onDelete('cascade')
+      )
   )
 
-  await createStandardTable(
-    db,
-    'facility_devices',
-    (qb) =>
-      qb.addColumn('serial_number', 'varchar(255)')
-        .addColumn('created_by', 'integer', (column) =>
-          column
-            .notNull()
-            .references('employment.id')
-            .onDelete('cascade'))
-        .addColumn('updated_by', 'integer', (column) =>
-          column
-            .references('employment.id')
-            .onDelete('cascade'))
-        .addColumn(
-          'device_id',
-          'integer',
-          (col) => col.notNull().references('devices.id').onDelete('cascade'),
-        )
-        .addColumn(
-          'facility_id',
-          'integer',
-          (col) =>
-            col.notNull().references('facilities.id').onDelete('cascade'),
-        ),
+  await createStandardTable(db, 'facility_devices', (qb) =>
+    qb
+      .addColumn('serial_number', 'varchar(255)')
+      .addColumn('created_by', 'integer', (column) =>
+        column.notNull().references('employment.id').onDelete('cascade')
+      )
+      .addColumn('updated_by', 'integer', (column) =>
+        column.references('employment.id').onDelete('cascade')
+      )
+      .addColumn('device_id', 'integer', (col) =>
+        col.notNull().references('devices.id').onDelete('cascade')
+      )
+      .addColumn('facility_id', 'integer', (col) =>
+        col.notNull().references('facilities.id').onDelete('cascade')
+      )
   )
+
+  await createStandardTable(db, 'procurers', (qb) =>
+    qb.addColumn('name', 'varchar(255)')
+  )
+
+  await createStandardTable(db, 'consumables', (qb) =>
+    qb.addColumn('name', 'varchar(255)').addColumn('is_medication', 'boolean')
+  )
+
+  await createStandardTable(db, 'facility_consumables', (qb) =>
+    qb
+      .addColumn('quantity_on_hand', 'integer')
+      .addColumn('facility_id', 'integer', (col) =>
+        col.notNull().references('facilities.id').onDelete('cascade')
+      )
+      .addColumn('consumable_id', 'integer', (col) =>
+        col.notNull().references('consumables.id').onDelete('cascade')
+      )
+      .addCheckConstraint(
+        'facility_consumables_quantity_on_hand',
+        sql`
+          quantity_on_hand >= 0
+        `
+      )
+  )
+
+  await createStandardTable(db, 'procurement', (qb) =>
+    qb
+      .addColumn('facility_id', 'integer', (col) =>
+        col.notNull().references('facilities.id').onDelete('cascade')
+      )
+      .addColumn('quantity', 'integer')
+      .addColumn('consumable_id', 'integer', (col) =>
+        col.notNull().references('consumables.id').onDelete('cascade')
+      )
+      .addCheckConstraint(
+        'consumption_quantity',
+        sql`
+        quantity >= 0
+       `
+      )
+  )
+
+  await createStandardTable(db, 'consumption', (qb) =>
+    qb
+      .addColumn('facility_id', 'integer', (col) =>
+        col.notNull().references('facilities.id').onDelete('cascade')
+      )
+      .addColumn('quantity', 'integer')
+      .addColumn('consumable_id', 'integer', (col) =>
+        col.notNull().references('consumables.id').onDelete('cascade')
+      )
+      .addCheckConstraint(
+        'consumption_quantity',
+        sql`
+        quantity >= 0
+      `
+      )
+  )
+
+  // await db.schema
+  //   .alterTable('manufactured_medications')
+  //   .addColumn('consumable_id', 'integer', (col) =>
+  //     col.references('consumables.id')
+  //   )
+  //   .execute()
+
+    await seedConsumablesFromMedications(db);
 }
 
 export async function down(db: Kysely<unknown>) {
   await db.schema.dropTable('facility_devices').execute()
   await db.schema.dropTable('device_capabilities').execute()
   await db.schema.dropTable('devices').execute()
+  // await db.schema
+  //   .alterTable('manufactured_medications')
+  //   .dropColumn('consumable_id')
+  //   .execute()
+  await db.schema.dropTable('facility_consumables').execute()
+  await db.schema.dropTable('consumption').execute()
+  await db.schema.dropTable('procurement').execute()
+  await db.schema.dropTable('devices').execute()
+  await db.schema.dropTable('consumables').execute()
+  await db.schema.dropTable('procurers').execute()
+}
+
+// deno-lint-ignore no-explicit-any
+async function seedConsumablesFromMedications(db: Kysely<any>) {
+  const medications = await db
+    .selectFrom('manufactured_medications')
+    .select('applicant_name')
+    .execute()
+  // await db
+  //   .insertInto('consumables')
+  //   .values(medications.map((c) => ({ name: c.applicant_name, is_medication: true })))
+  //   .execute()
+  // const consumables = await db
+  //   .selectFrom('manufactured_medications')
+  //   .select(['id', 'name'])
+  //   .execute()
+
+  // await inParallel.forEach(consumables, async (consumable) => {
+  //   await db
+  //     .updateTable('manufactured_medications')
+  //     .set('consumable_id', consumable.id)
+  //     .where('applicant_name', '=', consumable.name)
+  //     .execute()
+  // })
 }
