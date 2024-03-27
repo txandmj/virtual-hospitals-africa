913e78e874812c1c4bb52f0c033529d4430fd7ee
Chun <cpang26@my.bcit.ca>
2024-03-27 00:35:44 -0700

    made route to find patients based on waiting room and return href as part of the search result

--- routes/app/facilities/[facility_id]/patients.tsx
+++ routes/app/facilities/[facility_id]/patients.tsx
@@ -7,6 +7,7 @@ import {
 } from '../../../../types.ts'
 import PatientsView from '../../../../components/patients/View.tsx'
 import { getAllWithNames } from '../../../../db/models/patients.ts'
+import * as waiting_room from '../../../../db/models/waiting_room.ts'
 import { json } from '../../../../util/responses.ts'
 import { assertOr404 } from '../../../../util/assertOr.ts'
 
@@ -17,13 +18,24 @@ type PatientsProps = {
 
 export const handler: LoggedInHealthWorkerHandlerWithProps<PatientsProps> = {
   async GET(req, ctx) {
+    assertOr404(req.headers.get('accept') === 'application/json', 'We only accept JSON')
     const search = ctx.url.searchParams.get('search')
+    const facility_id = parseInt(ctx.params.facility_id)
+    const room = await waiting_room.get(ctx.state.trx, { facility_id })
 
     const patients = await getAllWithNames(ctx.state.trx, search)
 
-    assertOr404(req.headers.get('accept') === 'application/json', 'We only accept JSON')
+    const patients_with_href = patients.map((patient) => {
+      const patient_in_waiting_room = room.find((entry) => entry.patient.id === patient.id)
+      if (patient_in_waiting_room) {
+        return {id: patient.id , name: patient.name, href: `/app/patients/${patient.id}`}
+      } else{
+        return {id: patient.id , name: patient.name, href: `/app/facilities/${facility_id}/waiting_room/add?patient_id=${patient?.id}`}
+      }
+    })
 
-    return json(patients)
+
+    return json(patients_with_href)
 
   },
 }
