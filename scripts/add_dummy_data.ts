0a226bd8718c72c42f3711ec073a814d9d7d9aee
Will Weiss <will.weiss1230@gmail.com>
2024-04-03 20:53:23 -0400

    dummy data includes dob and avatar

--- scripts/add_dummy_data.ts
+++ scripts/add_dummy_data.ts
@@ -1,4 +1,6 @@
 import db from '../db/db.ts'
+import * as media from '../db/models/media.ts'
+import * as patients from '../db/models/patients.ts'
 import * as patient_encounters from '../db/models/patient_encounters.ts'
 import { addTestHealthWorker } from '../test/web/utilities.ts'
 import { randomZimbabweanDemographics } from '../util/zimbabweanDemographics.ts'
@@ -33,6 +35,12 @@ function randomReason(gender: 'female' | 'male'): EncounterReason {
   Add 3 nurses and 25 patients at various stages of intake & consultation
 */
 
+function randomAvatar(gender: 'male' | 'female') {
+  return `./static/images/avatars/random/${gender}/${
+    1 + Math.floor(Math.random() * 10)
+  }.png`
+}
+
 async function addDummyData() {
   const hw1 = await addTestHealthWorker(db, {
     scenario: 'approved-nurse',
@@ -55,15 +63,36 @@ async function addDummyData() {
   const hws = [hw1, hw2, hw3]
   const patient_indexes = shuffle(range(0, 25)).slice(0, 3)
 
-  for (let i = 0; i < 25; i++) {
+  const avatars_used = new Set<string>()
+
+  for (let i = 0; i < 10; i++) {
     const demo = randomZimbabweanDemographics()
+    let random_avatar = randomAvatar(demo.gender)
+    while (avatars_used.has(random_avatar)) {
+      random_avatar = randomAvatar(demo.gender)
+    }
+    avatars_used.add(random_avatar)
+
+    const file = Deno.readFileSync(random_avatar)
+    const inserted_media = await media.insert(db, {
+      mime_type: 'image/png',
+      binary_data: file,
+    })
+
     const reason = randomReason(demo.gender)
 
     const hw_index = patient_indexes.indexOf(i)
     const health_worker = hw_index !== -1 ? hws[hw_index] : null
 
+    const patient = await patients.upsert(db, {
+      name: `${demo.first_name} ${demo.last_name}`,
+      date_of_birth: randomDateOfBirth(),
+      gender: demo.gender,
+      avatar_media_id: inserted_media.id,
+    })
+
     await patient_encounters.upsert(db, 1, {
-      patient_name: demo.name,
+      patient_id: patient.id,
       reason,
       provider_ids: health_worker ? [health_worker.employee_id!] : [],
     })
