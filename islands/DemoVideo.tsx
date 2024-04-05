15bdfe7e2139962dcfc331d1e707958086fdd07a
Will Weiss <will.weiss1230@gmail.com>
2024-04-05 01:51:22 -0400

    functioning youtube

--- islands/DemoVideo.tsx
+++ islands/DemoVideo.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react'
+import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { useEffect } from 'preact/hooks'
 import { effect, useSignal } from '@preact/signals'
@@ -7,18 +7,25 @@ import { assert } from 'std/assert/assert.ts'
 const videoId = '8aB1BibAl18'
 const videoQuality = 'hd1440'
 
+// deno-lint-ignore no-explicit-any
+function getYT(window: any) {
+  if ('YT' in window) return window.YT
+}
+
 export default function DemoVideo() {
   const youtube_script_loaded = useSignal(false)
-  const youTubeIframeOnPage = useSignal(false)
+  const youtube_demo_iframe_on_page = useSignal(false)
+  const autoplay = useSignal(false)
+  // deno-lint-ignore no-explicit-any
   const player = useSignal<any>(null)
   const open = useSignal(false)
 
   function onYouTubePlayerAPIReady() {
-    assert('YT' in window, 'YouTube API not loaded')
-    const YT = window.YT as unknown as any
-    const el = document.getElementById('youTubeIframe')
+    assert('YT' in globalThis, 'YouTube API not loaded')
+    const el = document.getElementById('youtube_demo_iframe')
     assert(el)
-    return new YT.Player('youTubeIframe', {
+    const YT = getYT(globalThis)!
+    return new YT.Player('youtube_demo_iframe', {
       height: '390',
       width: '640',
       videoId,
@@ -29,80 +36,95 @@ export default function DemoVideo() {
   }
 
   useEffect(() => {
-    const hash = window.location.hash
+    // Weirdly, even if YT is loaded, the YT.Player might not be yet
+    function checkIfPlayerPresent() {
+      if (getYT(globalThis).Player) {
+        youtube_script_loaded.value = true
+      } else {
+        requestAnimationFrame(checkIfPlayerPresent)
+      }
+    }
+
+    const hash = globalThis.location.hash
     if (hash === '#demo') {
       open.value = true
     }
     self.addEventListener('hashchange', () => {
-      open.value = window.location.hash === '#demo'
+      open.value = globalThis.location.hash === '#demo'
+      autoplay.value = true
     })
-    if ('YT' in window) {
-      youtube_script_loaded.value = true
-    } else {
-      self.addEventListener('load', (event) => {
-        if (
-          event.target instanceof HTMLScriptElement &&
-          event.target.src === 'https://youtube.com/iframe_api'
-        ) {
-          youtube_script_loaded.value = true
-        }
-      })
-    }
+
+    if (getYT(globalThis)) return checkIfPlayerPresent()
+
+    self.addEventListener('load', (event) => {
+      if (
+        event.target === document
+      ) {
+        return youtube_script_loaded.value = true
+      }
+      if (
+        event.target instanceof HTMLScriptElement &&
+        event.target.src === 'https://youtube.com/iframe_api'
+      ) {
+        checkIfPlayerPresent()
+      }
+    })
   }, [])
 
+  // Sets youtube_demo_iframe_on_page when the youtube demo iframe is added to the page
   effect(() => {
     if (!youtube_script_loaded.value) return
-    if (document.getElementById('youTubeIframe')) {
-      youTubeIframeOnPage.value = true
-    } else {
-      // Select the target node
-      const targetNode = document.body // You can specify any parent element you want to observe
-
-      // Options for the observer (which mutations to observe)
-      const config = { childList: true, subtree: true }
-
-      // Create an observer instance linked to the callback function
-      const observer = new MutationObserver(
-        function handleInsertion(mutationsList, observer) {
-          for (const mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-              mutation.addedNodes.forEach((node) => {
-                if (
-                  node instanceof HTMLElement &&
-                  node.querySelector('#youTubeIframe')
-                ) {
-                  youTubeIframeOnPage.value = true
-                  observer.disconnect() // Disconnect the observer if needed
-                }
-              })
-            }
-          }
-        },
-      )
-
-      // Start observing the target node for configured mutations
-      observer.observe(targetNode, config)
+    if (document.getElementById('youtube_demo_iframe')) {
+      return youtube_demo_iframe_on_page.value = true
     }
+
+    const observer = new MutationObserver(
+      function handleInsertion(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach((node) => {
+              if (
+                node instanceof HTMLElement &&
+                node.querySelector('#youtube_demo_iframe')
+              ) {
+                youtube_demo_iframe_on_page.value = true
+                observer.disconnect()
+              }
+            })
+          }
+        }
+      },
+    )
+
+    observer.observe(document.body, { childList: true, subtree: true })
   })
 
   effect(() => {
-    if (youTubeIframeOnPage.value && !player.value) {
+    if (
+      youtube_demo_iframe_on_page.value && youtube_script_loaded.value &&
+      !player.value
+    ) {
       player.value = onYouTubePlayerAPIReady()
     }
   })
 
   effect(() => {
-    if (open.value && player.value) {
-      setTimeout(() => {
-        console.log('IN HERE', player.value, player.value!.playVideo)
-        player.value!.playVideo()
-      }, 200)
+    function checkIfCanPlayThenPlay() {
+      if (player.value.playVideo) {
+        player.value.playVideo(videoId, 0, videoQuality)
+      } else {
+        requestAnimationFrame(checkIfCanPlayThenPlay)
+      }
+    }
+
+    if (open.value && player.value && autoplay.value) {
+      checkIfCanPlayThenPlay()
     }
   })
 
   effect(() => {
     if (!open.value) {
-      youTubeIframeOnPage.value = false
+      youtube_demo_iframe_on_page.value = false
       player.value = null
     }
   })
@@ -111,7 +133,7 @@ export default function DemoVideo() {
     <Transition.Root show={open.value} as={Fragment}>
       <Dialog
         className='relative z-10'
-        onClose={() => window.location.hash = ''}
+        onClose={() => globalThis.location.hash = ''}
       >
         <Transition.Child
           as={Fragment}
@@ -126,7 +148,7 @@ export default function DemoVideo() {
         </Transition.Child>
 
         <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
-          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+          <div className='flex min-h-full justify-center p-4 text-center items-center sm:p-0'>
             <Transition.Child
               as={Fragment}
               enter='ease-out duration-300'
@@ -136,8 +158,11 @@ export default function DemoVideo() {
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative transform rounded-lg shadow-xl transition-all max-h-screen'>
-                <div className='embed-responsive-item' id='youTubeIframe' />
+              <Dialog.Panel className='relative transform rounded-lg shadow-xl transition-all max-h-screen max-w-screen'>
+                <div
+                  id='youtube_demo_iframe'
+                  className='embed-responsive-item'
+                />
               </Dialog.Panel>
             </Transition.Child>
           </div>
