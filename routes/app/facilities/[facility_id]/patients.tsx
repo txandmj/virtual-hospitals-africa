5b737bf4f8e439d962f6128cf96860cce62d65f9
Will Weiss <will.weiss1230@gmail.com>
2024-04-10 10:08:51 -0400

    use patient.href for optionHref

--- routes/app/facilities/[facility_id]/patients.tsx
+++ routes/app/facilities/[facility_id]/patients.tsx
@@ -1,11 +1,8 @@
-import { PageProps } from '$fresh/server.ts'
-import Layout from '../../../../components/library/Layout.tsx'
 import {
   EmployedHealthWorker,
   LoggedInHealthWorkerHandlerWithProps,
   RenderedPatient,
 } from '../../../../types.ts'
-import PatientsView from '../../../../components/patients/View.tsx'
 import { getAllWithNames } from '../../../../db/models/patients.ts'
 import * as waiting_room from '../../../../db/models/waiting_room.ts'
 import { json } from '../../../../util/responses.ts'
@@ -18,7 +15,10 @@ type PatientsProps = {
 
 export const handler: LoggedInHealthWorkerHandlerWithProps<PatientsProps> = {
   async GET(req, ctx) {
-    assertOr404(req.headers.get('accept') === 'application/json', 'We only accept JSON')
+    assertOr404(
+      req.headers.get('accept') === 'application/json',
+      'We only accept JSON',
+    )
     const search = ctx.url.searchParams.get('search')
     const facility_id = parseInt(ctx.params.facility_id)
     const room = await waiting_room.get(ctx.state.trx, { facility_id })
@@ -26,16 +26,26 @@ export const handler: LoggedInHealthWorkerHandlerWithProps<PatientsProps> = {
     const patients = await getAllWithNames(ctx.state.trx, search)
 
     const patients_with_href = patients.map((patient) => {
-      const patient_in_waiting_room = room.find((entry) => entry.patient.id === patient.id)
+      const patient_in_waiting_room = room.some((entry) =>
+        entry.patient.id === patient.id
+      )
+
       if (patient_in_waiting_room) {
-        return {id: patient.id , name: patient.name, href: `/app/patients/${patient.id}`}
-      } else{
-        return {id: patient.id , name: patient.name, href: `/app/facilities/${facility_id}/waiting_room/add?patient_id=${patient?.id}`}
+        return {
+          id: patient.id,
+          name: patient.name,
+          href: `/app/patients/${patient.id}`,
+        }
+      } else {
+        return {
+          id: patient.id,
+          name: patient.name,
+          href:
+            `/app/facilities/${facility_id}/waiting_room/add?patient_id=${patient?.id}`,
+        }
       }
     })
 
-
     return json(patients_with_href)
-
   },
 }
