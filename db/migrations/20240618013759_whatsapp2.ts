1de115e8a4545b2742ed5245024ab8f890cb968a
Will Weiss <will.weiss1230@gmail.com>
2024-06-17 19:49:06 -0400

    add additional tables to join to

---
+++ db/migrations/20240618013759_whatsapp2.ts
@@ -0,0 +1,36 @@
+import { Kysely, sql } from "kysely";
+import patientConversationStates from '../../chatbot/patient/conversationStates.ts'
+import pharmacistConversationStates from '../../chatbot/pharmacist/conversationStates.ts'
+import { createStandardTable } from '../createStandardTable.ts'
+
+export async function up(db: Kysely<unknown>) {
+  await db.schema
+    .createType('patient_conversation_state')
+    .asEnum(Object.keys(patientConversationStates))
+    .execute()
+
+  await createStandardTable(db, 'patient_whatsapp_messages_received', qb => 
+    qb.addColumn('whatsapp_message_received_id', 'uuid', col => col.references('whatsapp_messages_received.id').onDelete('cascade'))
+      .addColumn('patient_id', 'uuid', col => col.references('patients.id').onDelete('cascade'))
+      .addColumn('conversation_state', sql`patient_conversation_state`)
+    )
+
+  await db.schema
+    .createType('pharmacist_conversation_state')
+    .asEnum(Object.keys(pharmacistConversationStates))
+    .execute()
+
+  await createStandardTable(db, 'pharmacist_whatsapp_messages_received', qb => 
+    qb.addColumn('whatsapp_message_received_id', 'uuid', col => col.references('whatsapp_messages_received.id').onDelete('cascade'))
+      .addColumn('pharmacist_id', 'uuid', col => col.references('patients.id').onDelete('cascade'))
+      .addColumn('conversation_state', sql`pharmacist_conversation_state`)
+    )
+}
+
+export async function down(db: Kysely<unknown>) {
+  await db.schema.dropTable('pharmacist_whatsapp_messages_received')
+  await db.schema.dropType('pharmacist_conversation_state')
+  
+  await db.schema.dropTable('patient_whatsapp_messages_received')
+  await db.schema.dropType('patient_conversation_state')
+}
