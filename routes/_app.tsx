a203a98796d926e6b28952614b3f1f1368a07031
Will Weiss <will.weiss1230@gmail.com>
2024-06-19 11:31:00 -0400

    make it so we can represent arbitrary things to "send to"

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,42 +8,42 @@ export default function App({ Component, ...props }: PageProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Virtual Hospitals Africa</title>
         <link rel='stylesheet' href='/styles.css' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          rel='preconnect'
+          href='https://cdn.fontshare.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
+        />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Ubuntu:wght@100..900&display=swap'
+        />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Roboto'
+        />
+        <link
+          rel='stylesheet'
+          href='https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@800,500,700&display=swap'
+        />
         {props.route === '/' && (
           <>
             <meta
               name='description'
               content='Bringing accessible health care to Africans'
             />
-            <link rel='preconnect' href='https://fonts.googleapis.com' />
-            <link
-              rel='preconnect'
-              href='https://fonts.gstatic.com'
-              crossOrigin='anonymous'
-            />
-            <link
-              rel='preconnect'
-              href='https://cdn.fontshare.com'
-              crossOrigin='anonymous'
-            />
-            <link
-              rel='stylesheet'
-              href='https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
-            />
-            <link
-              rel='stylesheet'
-              href='https://fonts.googleapis.com/css2?family=Ubuntu:wght@100..900&display=swap'
-            />
-            <link
-              rel='stylesheet'
-              href='https://fonts.googleapis.com/css2?family=Roboto'
-            />
             <link
               rel='stylesheet'
               href='/mhi.css'
-            />
-            <link
-              rel='stylesheet'
-              href='https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@800,500,700&display=swap'
             />
             <meta property='og:type' content='website' />
             <meta property='og:title' content='Virtual Hospitals Africa' />
