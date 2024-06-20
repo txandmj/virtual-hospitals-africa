d7a1916acbaf3c289d89899f2f5a03da7053856a
alicejuancc <iq23062@bristol.ac.uk>
2024-06-20 22:53:57 +0100

    Adjust color and font for send-to-menu

--- islands/SendToMenu.tsx
+++ islands/SendToMenu.tsx
@@ -8,7 +8,6 @@ import cls from '../util/cls.ts'
 import { ComponentChild } from 'preact'
 
 
-
 type Sendable =
   & {
     image: {
@@ -202,7 +201,7 @@ export function SendableComponent(
                 {name}
               </p>
               {description && (
-                <p className='truncate text-xs text-gray-900'>
+                <p className='truncate text-xs text-gray-500'>
                   {description.parenthetical === 'address' ? (
                     <a
                       href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
@@ -210,6 +209,7 @@ export function SendableComponent(
                       )}`}
                       target='_blank'
                       rel='noopener noreferrer'
+                      className='text-blue-500'
                     >
                       {description.text}
                     </a>
@@ -267,15 +267,15 @@ export default function SendToMenu() {
                 >
                   <Dialog.Panel className='pointer-events-auto fixed right-0 top-0 h-full w-[448px] bg-white shadow-xl'>
                     <div className='flex h-full flex-col overflow-y-scroll bg-white shadow-xl'>
-                      <div className='p-6'>
+                      <div className='p-6 bg-indigo-700'>
                         <div className='flex items-start justify-between'>
-                          <Dialog.Title className='text-base font-semibold leading-6 text-gray-900'>
-                            Team
+                          <Dialog.Title className='text-base font-semibold leading-6 text-white'>
+                            Send to
                           </Dialog.Title>
                           <div className='ml-3 flex h-7 items-center'>
                             <button
                               type='button'
-                              className='relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500'
+                              className='relative rounded-md bg-indigo-700 text-gray-200 hover:text-gray-500 focus:ring-2 focus:ring-indigo-700'
                               onClick={() => setOpen(false)}
                             >
                               <span className='absolute -inset-2.5' />
