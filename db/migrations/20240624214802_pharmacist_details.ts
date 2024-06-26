81afca9b80b76ebf5b778d017eb24709dd69c0d0
lovesmallpaes <mike.huang.mikank@gmail.com>
2024-06-26 18:16:52 +0100

    Updated address column so that it reference the address table

--- db/migrations/20240624214802_pharmacist_details.ts
+++ db/migrations/20240624214802_pharmacist_details.ts
@@ -23,7 +23,8 @@ export async function up(db: Kysely<unknown>) {
     ])
     .execute()
 
-  await db.schema.alterTable('pharmacists')
+  await db.schema
+    .alterTable('pharmacists')
     .dropColumn('registration_number')
     .dropColumn('id_number')
     .dropColumn('name')
@@ -31,7 +32,9 @@ export async function up(db: Kysely<unknown>) {
     .addColumn('prefix', sql`name_prefix`)
     .addColumn('given_name', 'varchar(255)', (col) => col.notNull())
     .addColumn('family_name', 'varchar(255)', (col) => col.notNull())
-    .addColumn('address', 'varchar(255)')
+    .addColumn('address_id', 'uuid', (col) =>
+      col.references('Address.id').onDelete('set null')
+    )
     .addColumn('town', 'varchar(255)')
     .addColumn('expiry_date', 'date', (col) => col.notNull())
     .addColumn('pharmacist_type', sql`pharmacist_type`, (col) => col.notNull())
@@ -39,7 +42,8 @@ export async function up(db: Kysely<unknown>) {
 }
 
 export async function down(db: Kysely<unknown>) {
-  await db.schema.alterTable('pharmacists')
+  await db.schema
+    .alterTable('pharmacists')
     .addColumn('registration_number', 'varchar(255)')
     .addColumn('id_number', 'varchar(255)')
     .addColumn('name', 'varchar(255)')
@@ -47,7 +51,7 @@ export async function down(db: Kysely<unknown>) {
     .dropColumn('prefix')
     .dropColumn('given_name')
     .dropColumn('family_name')
-    .dropColumn('address')
+    .dropColumn('address_id')
     .dropColumn('town')
     .dropColumn('expiry_date')
     .dropColumn('pharmacist_type')
