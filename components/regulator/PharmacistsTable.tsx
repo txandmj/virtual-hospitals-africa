eb75da9e35891e72148e0e33eb69dce3510ac3c9
Will Weiss <will.weiss1230@gmail.com>
2024-07-02 20:38:30 -0400

    fixes

---
+++ components/regulator/PharmacistsTable.tsx
@@ -0,0 +1,95 @@
+import { JSX } from 'preact'
+import Table from '../library/Table.tsx'
+import { TableColumn } from '../library/Table.tsx'
+import { Button } from '../library/Button.tsx'
+import FormRow from '../../islands/form/Row.tsx'
+import { SearchInput } from '../../islands/form/Inputs.tsx'
+import { UserCircleIcon } from '../library/icons/heroicons/outline.tsx'
+import { EmptyState } from '../library/EmptyState.tsx'
+
+type Pharmacist = {
+  license_number: string
+  prefix: string | 'Mr' | 'Mrs' | 'Ms' | 'Dr' | 'Miss' | 'Sr'
+  given_name: string
+  family_name: string
+  address: string
+  town: string
+  expiry_date: string
+  pharmacist_type:
+    | 'Dispensing Medical Practitioner'
+    | 'Ind Clinic Nurse'
+    | 'Pharmacist'
+    | 'Sales Representative'
+    | 'Pharmacy Technician'
+    | 'Veterinary Surgeon'
+}
+
+type PharmacistsTableProps = {
+  pharmacists: Pharmacist[]
+  pathname: string
+}
+
+export default function PharmacistsTable({
+  pharmacists,
+  pathname,
+}: PharmacistsTableProps): JSX.Element {
+  const columns: TableColumn<Pharmacist>[] = [
+    {
+      label: 'Prefix',
+      data: 'prefix',
+    },
+    {
+      label: 'Given Name',
+      data: 'given_name',
+    },
+    {
+      label: 'Family Name',
+      data: 'family_name',
+    },
+    {
+      label: 'Address',
+      data: 'address',
+    },
+    {
+      label: 'Town',
+      data: 'town',
+    },
+    {
+      label: 'License Number',
+      data: 'license_number',
+    },
+    {
+      label: 'Expiry Date',
+      data: 'expiry_date',
+    },
+    {
+      label: 'Pharmacist Type',
+      data: 'pharmacist_type',
+    },
+  ]
+  return (
+    <>
+      <FormRow className='mb-4'>
+        <SearchInput />
+        <Button
+          type='button'
+          href={`${pathname}/invite`}
+          className='w-max rounded-md border-0 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 h-9 p-2 self-end whitespace-nowrap grid place-items-center'
+        >
+          Invite
+        </Button>
+      </FormRow>
+      <Table
+        columns={columns}
+        rows={pharmacists}
+        EmptyState={() => (
+          <EmptyState
+            header='No pharmacists'
+            explanation='Invite a pharmacist to get started'
+            Icon={UserCircleIcon}
+          />
+        )}
+      />
+    </>
+  )
+}
--- routes/regulator/pharmacists.tsx
+++ routes/regulator/pharmacists.tsx
@@ -1,9 +1,9 @@
 import Layout from '../../components/library/Layout.tsx'
-import PharmacistsTable from '../../components/health_worker/PharmacistsTable.tsx'
+import PharmacistsTable from '../../components/regulator/PharmacistsTable.tsx'
 import { LoggedInRegulator } from '../../types.ts'
 
 const pharmacists = [
-  { 
+  {
     license_number: '123456',
     prefix: 'Ms',
     given_name: 'Aaa',
@@ -11,9 +11,9 @@ const pharmacists = [
     address: '123 Main St xxxxxx',
     town: 'Aaa',
     expiry_date: '2025-12-31',
-    pharmacist_type: 'Pharmacist'
+    pharmacist_type: 'Pharmacist',
   },
-  { 
+  {
     license_number: '234567',
     prefix: 'Miss',
     given_name: 'Bbb',
@@ -21,15 +21,20 @@ const pharmacists = [
     address: '456 Dean St yyyyyyyyy',
     town: 'Bbb',
     expiry_date: '2025-12-31',
-    pharmacist_type: 'Pharmacist'
-  }
+    pharmacist_type: 'Pharmacist',
+  },
 ]
 
+// deno-lint-ignore require-await
 export default async function PharmacistsPage(
-  req: Request,
-  ctx: { route: string, url: URL, state: { regulator: LoggedInRegulator['regulator'] } }
+  _req: Request,
+  ctx: {
+    route: string
+    url: URL
+    state: { regulator: LoggedInRegulator['regulator'] }
+  },
 ) {
-  const regulator = ctx.state.regulator;
+  const regulator = ctx.state.regulator
 
   return (
     <Layout
@@ -42,6 +47,7 @@ export default async function PharmacistsPage(
     >
       <PharmacistsTable
         pharmacists={pharmacists}
+        pathname={ctx.url.pathname}
       />
     </Layout>
   )
