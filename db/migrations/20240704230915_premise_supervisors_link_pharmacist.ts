4d3e267e05f30a2107e00529e2f30a6660420aa2
lovesmallpaes <mike.huang.mikank@gmail.com>
2024-07-04 23:13:50 +0100

    Formatted code

--- db/migrations/20240704230915_premise_supervisors_link_pharmacist.ts
+++ db/migrations/20240704230915_premise_supervisors_link_pharmacist.ts
@@ -3,8 +3,10 @@ import { Kysely } from 'kysely'
 export async function up(db: Kysely<unknown>) {
   await db.schema
     .alterTable('premise_supervisors')
-    .addColumn('pharmacist_id', 'uuid', (col) =>
-      col.references('pharmacists.id').onDelete('cascade')
+    .addColumn(
+      'pharmacist_id',
+      'uuid',
+      (col) => col.references('pharmacists.id').onDelete('cascade'),
     )
     .execute()
 }
