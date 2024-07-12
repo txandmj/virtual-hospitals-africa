aa57b6f85001829e3998f959e60433bfd49f4e47
CHENQIYUAN <ty23828@bristol.ac.uk>
2024-07-13 00:56:31 +0100

    try to adjust radio button

--- islands/SendTo/RequestRadioButtons.tsx
+++ islands/SendTo/RequestRadioButtons.tsx
@@ -1,34 +1,82 @@
+import { useState } from 'react';
+import { MakeAppointments } from '../../components/library/icons/MakeAppointments.tsx';
+import { DeclareEmergency } from '../../components/library/icons/DeclareEmergency.tsx';
+import { RequestReview } from '../../components/library/icons/RequestReview.tsx';
+import { PickedMakeAppointments } from '../../components/library/icons/PickedMakeAppointments.tsx';
+import { PickedDeclareEmergency } from '../../components/library/icons/PickedDeclareEmergency.tsx';
+import { PickedRequestReview } from '../../components/library/icons/PickedRequestReview.tsx';
+
 const REQUEST_TYPES = {
   request_visit: 'Request Visit',
   request_review: 'Request Review',
   make_appointment: 'Make Appointment',
   declare_emergency: 'Declare Emergency',
-}
+};
 
-type RequestType = keyof typeof REQUEST_TYPES
+type RequestType = keyof typeof REQUEST_TYPES;
+
+const DefaultIconComponents = {
+  request_visit: <RequestReview />,
+  request_review: <RequestReview />,
+  make_appointment: <MakeAppointments />,
+  declare_emergency: <DeclareEmergency />,
+};
+
+const PickedIconComponents = {
+  request_visit: <PickedRequestReview />,
+  request_review: <PickedRequestReview />,
+  make_appointment: <PickedMakeAppointments />,
+  declare_emergency: <PickedDeclareEmergency />,
+};
 
 export function SendToRequestRadioButtons({
   form,
   options,
 }: {
-  form: string
-  options: RequestType[]
+  form: string;
+  options: RequestType[];
 }) {
+  const [selectedOption, setSelectedOption] = useState<RequestType | null>(null);
+
+  const handleChange = (option: RequestType) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <fieldset className='flex flex-col gap-2'>
-      <div className='flex flex-col gap-1'>
+    <fieldset className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
         {options.map((option) => (
-          <label key={option} className='flex items-center gap-2'>
+          <label key={option} className="flex items-center gap-2">
             <input
               form={form}
-              type='radio'
-              name='send_to.request_type'
+              type="radio"
+              name="send_to.request_type"
               value={option}
+              checked={selectedOption === option}
+              onChange={() => handleChange(option)}
+              className="hidden"
             />
-            <span>{REQUEST_TYPES[option]}</span>
+            <div
+              className={`flex items-center gap-2 p-2 cursor-pointer rounded-md ${
+                selectedOption === option ? 'bg-white' : ''
+              }`}
+              onClick={() => handleChange(option)}
+            >
+              <div className="icon-container">
+                {selectedOption === option ? PickedIconComponents[option] : DefaultIconComponents[option]}
+              </div>
+              <span
+                className={`text-lg font-medium ${
+                  selectedOption === option ? 'underline' : ''
+                }`}
+              >
+                {REQUEST_TYPES[option]}
+              </span>
+            </div>
           </label>
         ))}
       </div>
     </fieldset>
-  )
+  );
 }
+
