e9e56bd448bd9ae11d9e2164e40821427142a72b
SeventyThree73 <2294387616@qq.com>
2024-07-24 21:53:38 +0100

    deno fmt

--- db/migrations/20240705162545_prescriptions.ts
+++ db/migrations/20240705162545_prescriptions.ts
@@ -9,37 +9,59 @@ export async function up(db: Kysely<unknown>) {
         'varchar(255)',
         (col) => col.notNull().unique(),
       )
-      .addColumn('prescriber_id', 'uuid', (col) => col.notNull().references('patient_encounter_providers.id').onDelete('cascade'),)
-      .addColumn('patient_id', 'uuid', (col) => col.references('patients.id').onDelete('cascade'),)
-    )
+      .addColumn('prescriber_id', 'uuid', (col) =>
+        col.notNull().references('patient_encounter_providers.id').onDelete(
+          'cascade',
+        ))
+      .addColumn('patient_id', 'uuid', (col) =>
+        col.references('patients.id').onDelete('cascade')))
 
-    await createStandardTable(db, 'patient_prescription_medications', (qb) =>
+  await createStandardTable(
+    db,
+    'patient_prescription_medications',
+    (qb) =>
       qb.addColumn(
         'patient_condition_medication_id',
         'uuid',
-        (col) => col.notNull().references('patient_condition_medications.id').onDelete('cascade'),
+        (col) =>
+          col.notNull().references('patient_condition_medications.id').onDelete(
+            'cascade',
+          ),
       )
         .addColumn(
           'prescription_id',
           'uuid',
-          (col) => col.notNull().references('prescriptions.id').onDelete('cascade'),
-        )
-    )
-  
-  await createStandardTable(db, 'patient_prescription_medications_filled', (qb) =>
+          (col) =>
+            col.notNull().references('prescriptions.id').onDelete('cascade'),
+        ),
+  )
+
+  await createStandardTable(
+    db,
+    'patient_prescription_medications_filled',
+    (qb) =>
       qb.addColumn(
         'patient_prescription_medication_id',
         'uuid',
-        (col) => col.notNull().references('patient_prescription_medications.id').onDelete('cascade'),
+        (col) =>
+          col.notNull().references('patient_prescription_medications.id')
+            .onDelete('cascade'),
       )
-        .addColumn('pharmacist_id', 'uuid', (col) =>
-          col.notNull().references('pharmacists.id').onDelete('cascade'),
+        .addColumn(
+          'pharmacist_id',
+          'uuid',
+          (col) =>
+            col.notNull().references('pharmacists.id').onDelete('cascade'),
         )
-        .addColumn('pharmacy_id', 'uuid', (col) =>
-          col.notNull().references('premises.id').onDelete('cascade'),
+        .addColumn(
+          'pharmacy_id',
+          'uuid',
+          (col) => col.notNull().references('premises.id').onDelete('cascade'),
         )
-        .addUniqueConstraint('patient_prescription_medication_id', ['patient_prescription_medication_id'])
-    )
+        .addUniqueConstraint('patient_prescription_medication_id', [
+          'patient_prescription_medication_id',
+        ]),
+  )
 }
 
 export async function down(db: Kysely<unknown>) {
