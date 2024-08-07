2b5bab145970ad4439b71b45230a082f388590db
Will Weiss <will.weiss1230@gmail.com>
2024-08-06 23:20:39 -0400

    slight changes

--- islands/SendTo/Form.tsx
+++ islands/SendTo/Form.tsx
@@ -1,7 +1,7 @@
 import { Button } from '../../components/library/Button.tsx'
+import words from '../../util/words.ts'
 import { SendToRequestRadioButtons } from './RequestRadioButtons.tsx'
 
-//export function SendToForm({ form }: { form: 'intake' | 'encounter' }) {
 export function SendToForm(
   { form, textarea, requestTypeOptions }: {
     form: 'intake' | 'encounter'
@@ -11,27 +11,24 @@ export function SendToForm(
 ) {
   return (
     <div className='flex flex-col'>
-      {textarea === 'additional_details' && requestTypeOptions && (
+      {requestTypeOptions && (
         <SendToRequestRadioButtons
           form={form}
           options={requestTypeOptions}
         />
       )}
-      <div className='mt-6 px-4'>
-        <h2 className='text-sm font-sans font-medium text-gray-900'>
-          {textarea === 'additional_details'
-            ? 'Additional Details'
-            : 'Reason for Escalation'}
-        </h2>
-        <textarea
-          form={form}
-          name={textarea === 'additional_details'
-            ? 'send_to.additional_details'
-            : 'send_to.reason_for_escalation'}
-          className='w-full border border-gray-300 rounded-md p-2 mt-2'
-        >
-        </textarea>
-      </div>
+      {textarea && (
+        <div className='mt-6 px-4'>
+          <h2 className='text-sm font-sans font-medium text-gray-900'>
+            {words(textarea).join(' ')}
+          </h2>
+          <textarea
+            form={form}
+            name={`send_to.${textarea}`}
+            className='w-full border border-gray-300 rounded-md p-2 mt-2'
+          />
+        </div>
+      )}
       <div className='mt-6 px-4 flex justify-end'>
         <Button form={form} type='submit' variant='solid' color='primary'>
           Send
