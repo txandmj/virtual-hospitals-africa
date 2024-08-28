f81fa3e0a88ad28662a854b0c8e9aa37534d523a
chen84613 <chen84613@gmail.com>
2024-08-28 02:57:00 +0100

    fixed bug

--- routes/app/drugs.json.tsx
+++ routes/app/drugs.json.tsx
@@ -3,6 +3,7 @@ import { LoggedInHealthWorkerHandlerWithProps } from '../../types.ts'
 
 export const handler: LoggedInHealthWorkerHandlerWithProps<unknown> = {
   GET(_req, ctx) {
-    return searchResponse(ctx)
+    const search = ctx.url.searchParams.get('search')
+    return searchResponse(search)
   },
 }
