541156ab24caedd5faefd8d2fc7c084dcc5e8eb1
alicejuancc <iq23062@bristol.ac.uk>
2024-08-29 19:05:07 +0100

    Fix fmt

--- components/patients/review/ConditionsForm.tsx
+++ components/patients/review/ConditionsForm.tsx
@@ -1,9 +1,9 @@
-import {PreExistingConditionWithDrugs } from '../../../types.ts'
+import { PreExistingConditionWithDrugs } from '../../../types.ts'
 import PreExistingConditionsForm from '../../../islands/pre-existing-conditions/DiagnosisForm.tsx'
 import FormSection from '../../library/FormSection.tsx'
 
 export default function DiagnosesConditions(
-  { pre_existing_conditions}: {
+  { pre_existing_conditions }: {
     pre_existing_conditions: PreExistingConditionWithDrugs[]
   },
 ) {
