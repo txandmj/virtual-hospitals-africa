4a1e618d6b975d594f0fb84a0483a04feaf007c8
chen84613 <chen84613@gmail.com>
2024-08-30 00:14:42 +0100

    fixed fmt

--- db/models/pharmacists.ts
+++ db/models/pharmacists.ts
@@ -89,9 +89,16 @@ function getQuery(trx: TrxOrDb) {
       'pharmacists.given_name',
       'pharmacists.family_name',
       nameSql('pharmacists').as('name'),
-      sql<string | null>`CASE WHEN pharmacists.address = 'LOCUM' THEN NULL ELSE pharmacists.address END`.as('address'),
+      sql<
+        string | null
+      >`CASE WHEN pharmacists.address = 'LOCUM' THEN NULL ELSE pharmacists.address END`
+        .as('address'),
       'pharmacists.town',
-      sql<string | null>`CASE WHEN pharmacists.address = 'LOCUM' THEN NULL ELSE ${addressDisplaySql('pharmacists')} END`.as('address_display'),
+      sql<
+        string | null
+      >`CASE WHEN pharmacists.address = 'LOCUM' THEN NULL ELSE ${
+        addressDisplaySql('pharmacists')
+      } END`.as('address_display'),
       sql<string>`'/regulator/pharmacists/' || pharmacists.id`.as('href'),
       sql<string>`TO_CHAR(pharmacists.expiry_date, 'YYYY-MM-DD')`.as(
         'expiry_date',
