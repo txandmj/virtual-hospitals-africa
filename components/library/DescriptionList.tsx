e51138bbd34c624ec73ee6b65074bb06bb72b305
laurencelo1 <llo7@dons.usfca.edu>
2024-11-06 12:54:49 -0800

    formatting

--- components/library/DescriptionList.tsx
+++ components/library/DescriptionList.tsx
@@ -36,15 +36,12 @@ function createRowElement(
     )
     const element = cell.edit_href
       ? (
-        <a
-          className={`inline-block ${cell.italics ? 'italic' : 'not-italic'}`}
-          href={cell.edit_href}
-        >
+        <a style={{ display: 'inline-block' }} href={cell.edit_href}>
           {cell.value}
         </a>
       )
       : (
-        <span className={cell.italics ? 'italic' : 'not-italic'}>
+        <span>
           {cell.value}
         </span>
       )
@@ -90,7 +87,7 @@ function createDividerElement(row_number: number): JSX.Element {
   return (
     <div
       className='border-b border-gray-200 mt-2 mb-2'
-      style={{ gridColumn: '1 / 3', gridRow: row_number }}
+      style={{ gridColumn: '1 / 4', gridRow: row_number }}
     >
     </div>
   )
@@ -118,26 +115,12 @@ export const DescriptionList = (
   let page_row_start: number = 0
   let page_row_end: number = 1
 
-/*
-  return (
-    <>
-      <div
-        style={{ gridColumn: 2, gridRow: row_number }}
-      >
-        {row_element}
-      </div>
-      <div
-        className='w-4 h-4 text-gray-500'
-        style={{ gridColumn: 3, gridRow: row_number }}
-      >
-        <PencilSquareIcon />
-      </div>
-    </>
-  )
-*/
-
   for (const page of pages) {
-    const titleElement = createTitleElement(page.title, page_row_start + 1, page_row_end)
+    const titleElement = createTitleElement(
+      page.title,
+      page_row_start + 1,
+      page_row_end,
+    )
     if (titleElement) elements.push(titleElement)
     page_row_start = page_row_end
 
@@ -145,18 +128,18 @@ export const DescriptionList = (
       elements.push(
         <>
           <div
-            className="italic"
+            className='italic'
             style={{ gridColumn: 2, gridRow: page_row_end }}
           >
             None provided
           </div>
-          <div 
-          className='w-4 h-4 text-gray-500'
-          style={{ gridColumn: 3, gridRow: page_row_end }}
+          <div
+            className='w-4 h-4 text-gray-500'
+            style={{ gridColumn: 3, gridRow: page_row_end }}
           >
             <PencilSquareIcon />
           </div>
-        </>
+        </>,
       )
       page_row_end += 1
     } else {
@@ -179,7 +162,11 @@ export const DescriptionList = (
     page_row_start = page_row_end
 
     for (const section of page.sections) {
-      const sectionTitleElement = createTitleElement(section.title, page_row_start, page_row_end)
+      const sectionTitleElement = createTitleElement(
+        section.title,
+        page_row_start,
+        page_row_end,
+      )
       if (sectionTitleElement) elements.push(sectionTitleElement)
       page_row_start = page_row_end
 
@@ -187,18 +174,18 @@ export const DescriptionList = (
         elements.push(
           <>
             <div
-              className="italic"
+              className='italic'
               style={{ gridColumn: 2, gridRow: page_row_end }}
             >
               None provided
             </div>
-            <div 
+            <div
               className='w-4 h-4 text-gray-500'
               style={{ gridColumn: 3, gridRow: page_row_end }}
-              >
-                <PencilSquareIcon />
+            >
+              <PencilSquareIcon />
             </div>
-          </>
+          </>,
         )
         page_row_end += 1
       } else {
@@ -228,4 +215,3 @@ export const DescriptionList = (
     </div>
   )
 }
-
