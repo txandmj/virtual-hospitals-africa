939b3a3ea6a9e7e62de0ac0b713a1e6c4863dc70
rishsanyal <rishabh106@gmail.com>
2024-11-25 18:13:09 -0800

    Fixing deno check issues

--- islands/patient-drawer/Drawer.tsx
+++ islands/patient-drawer/Drawer.tsx
@@ -31,7 +31,7 @@ export function PatientDrawer(
     findings: RenderedPatientExaminationFinding[]
     measurements: Measurement<keyof Measurements>[]
     sendables: Sendable[]
-    flaggedVitals?: Map<string, boolean>
+    flaggedVitals?: Map<string, number>
   },
 ) {
   return (
