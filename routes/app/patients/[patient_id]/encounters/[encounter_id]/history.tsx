f4adf958e2ba8de566233c9f88d6c3aac3223932
Will Weiss <will.weiss1230@gmail.com>
2025-01-30 20:30:49 -0500

    wip

--- routes/app/patients/[patient_id]/encounters/[encounter_id]/history.tsx
+++ routes/app/patients/[patient_id]/encounters/[encounter_id]/history.tsx
@@ -4,7 +4,7 @@ import { HistoryContext } from './history/_middleware.tsx'
 // deno-lint-ignore require-await
 export default async function (_req: Request, ctx: HistoryContext) {
   if (ctx.state.current_assessment) return ctx.next()
-
+  console.log(ctx.state)
   const goto_assessment = ctx.state.next_incomplete_assessment ||
     ctx.state.history_assessments[0]
   const url = new URL(ctx.url)
