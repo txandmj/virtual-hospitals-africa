78f3bbcc5c420dad46461b55785231abdb95e270
Will Weiss <will.weiss1230@gmail.com>
2025-06-26 14:37:23 -0400

    pages with fade-in-heart

---
+++ routes/fade-in-heart/indigo-on-transparent.tsx
@@ -0,0 +1,5 @@
+import { FadeInHeartPage } from '../../components/library/fade-in-heart/page.tsx'
+
+export default function () {
+  return <FadeInHeartPage primaryColor='indigo' />
+}
---
+++ routes/fade-in-heart/white-on-indigo.tsx
@@ -0,0 +1,5 @@
+import { FadeInHeartPage } from '../../components/library/fade-in-heart/page.tsx'
+
+export default function () {
+  return <FadeInHeartPage primaryColor='white' backgroundColor='#312E81' />
+}
