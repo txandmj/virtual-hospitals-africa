10836d5e096502b31fc8a8a85bfa827a00ee38de
Claudia Sabbadin <claudiasabb22@gmail.com>
2025-07-03 17:28:21 +0200

    full width submit button

--- components/library/ContactForm.tsx
+++ components/library/ContactForm.tsx
@@ -1,48 +1,50 @@
-import { Button } from './Button.tsx'
+import { Button } from "./Button.tsx";
 import {
   SelectWithOptions,
   TextArea,
   TextInput,
-} from '../../islands/form/Inputs.tsx'
-import FormRow from './FormRow.tsx'
-import Form from './Form.tsx'
+} from "../../islands/form/Inputs.tsx";
+import FormRow from "./FormRow.tsx";
+import Form from "./Form.tsx";
 
 export const CONTACT_REASON_OPTIONS = [
-  { value: 'general_inquiry' as const, label: 'General Inquiry' },
-  { value: 'book_a_demo' as const, label: 'Book a demo' },
-  { value: 'book_an_intro_call' as const, label: 'Book an intro call' },
-  { value: 'request_investor_deck' as const, label: 'Request investor deck' },
-]
+  { value: "general_inquiry" as const, label: "General Inquiry" },
+  { value: "book_a_demo" as const, label: "Book a demo" },
+  { value: "book_an_intro_call" as const, label: "Book an intro call" },
+  { value: "request_investor_deck" as const, label: "Request investor deck" },
+];
 
-export type ContactReason = (typeof CONTACT_REASON_OPTIONS)[number]['value']
+export type ContactReason = (typeof CONTACT_REASON_OPTIONS)[number]["value"];
 
 export default function ContactForm({ reason }: { reason: ContactReason }) {
   return (
     <Form
-      method='POST'
-      action='/interest'
-      className='w-full mt-4 min-w-[450px]'
+      method="POST"
+      action="/interest"
+      className="w-full mt-4 min-w-[450px]"
     >
-      <FormRow className='w-full'>
+      <FormRow className="w-full">
         <SelectWithOptions
-          name='reason'
+          name="reason"
           required
           value={reason}
           options={CONTACT_REASON_OPTIONS}
         />
       </FormRow>
-      <FormRow className='w-full'>
-        <TextInput name='name' required />
+      <FormRow className="w-full">
+        <TextInput name="name" required />
       </FormRow>
-      <FormRow className='w-full'>
-        <TextInput name='email' type='email' required />
+      <FormRow className="w-full">
+        <TextInput name="email" type="email" required />
       </FormRow>
-      <FormRow className='w-full'>
-        <TextArea name='message' rows={3} />
+      <FormRow className="w-full">
+        <TextArea name="message" rows={3} />
       </FormRow>
-      <FormRow className='w-full container mt-2'>
-        <Button type='submit'>Submit</Button>
+      <FormRow className="w-full container mt-2">
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
       </FormRow>
     </Form>
-  )
+  );
 }
