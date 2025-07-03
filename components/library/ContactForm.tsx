02b38465f27353ce185e91a551a8e1c983a30d9a
Claudia Sabbadin <claudiasabb22@gmail.com>
2025-07-03 17:32:39 +0200

    remove min width

--- components/library/ContactForm.tsx
+++ components/library/ContactForm.tsx
@@ -18,7 +18,7 @@ export type ContactReason = (typeof CONTACT_REASON_OPTIONS)[number]["value"];
 
 export default function ContactForm({ reason }: { reason: ContactReason }) {
   return (
-    <Form method="POST" action="/interest" className="w-full min-w-[450px]">
+    <Form method="POST" action="/interest" className="w-full">
       <FormRow className="w-full">
         <SelectWithOptions
           name="reason"
