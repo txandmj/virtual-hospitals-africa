9dc7d376b1d2b443177373379c0333b41d43c1cd
Will Weiss <will.weiss1230@gmail.com>
2025-08-17 10:41:26 -0400

    noindex

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component, ...props }: PageProps) {
       <head>
         <meta charset='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='robots' content='noindex' />
         <title>Virtual Hospitals Africa</title>
         <link rel='stylesheet' href='/styles.css' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
