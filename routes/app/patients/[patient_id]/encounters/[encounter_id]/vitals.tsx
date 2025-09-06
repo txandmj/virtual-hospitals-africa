6fb1a9541c11ba428f3740433e74a4243e5595e2
Ettore Mihaili <ettoremihaili@gmail.com>
2025-09-06 19:29:39 +0200

    vitals iteration
    Co-authored-by: William Weiss <william.t.weiss@gmail.com>

--- routes/app/patients/[patient_id]/encounters/[encounter_id]/vitals.tsx
+++ routes/app/patients/[patient_id]/encounters/[encounter_id]/vitals.tsx
@@ -1,91 +1,11 @@
-import { EncounterContext, EncounterPage } from './_middleware.tsx'
-import { z } from 'zod'
-import * as vitals from '../../../../../../db/models/vitals.ts'
-import * as patient_findings from '../../../../../../db/models/patient_measurements.ts'
-import { getRequiredUUIDParam } from '../../../../../../util/getParam.ts'
-import { completeStep } from './_middleware.tsx'
-import { postHandler } from '../../../../../../util/postHandler.ts'
-import { snomed_concept_id } from '../../../../../../util/validators.ts'
-import filterOfType from '../../../../../../util/filterOfType.ts'
-import { VitalsForm } from '../../../../../../components/vitals/Form.tsx'
-import { PRIORITIES } from '../../../../../../types.ts'
-import { assert } from 'std/assert/assert.ts'
-import { promiseProps } from '../../../../../../util/promiseProps.ts'
+import redirect from '../../../../../../util/redirect.ts'
+import { EncounterContext } from './_middleware.tsx'
 
-const VitalsSchema = z.object({
-  findings: z.record(
-    z.string().uuid(),
-    z.object({
-      snomed_concept_id,
-      value: z.number().positive().optional(),
-      units: z.string(),
-      priority: z.enum(PRIORITIES).optional().default('Normal'),
-      note: z.string().optional(),
-    }),
-  ).optional().transform((findings) =>
-    Object.entries(findings || {}).map((
-      [finding_id, { note, priority, ...finding }],
-    ) => ({
-      finding_id,
-      ...finding,
-      // Treat a Normal evaluation with no note as no evaluation
-      evaluation: note || priority !== 'Normal' ? { note, priority } : null,
-    }))
-  ),
-})
+// deno-lint-ignore require-await
+export default async function (_req: Request, ctx: EncounterContext) {
 
-function hasValue(
-  finding: { value?: number },
-): finding is { value: number } {
-  return typeof finding.value === 'number'
+  const url = new URL(ctx.url)
+
+  url.pathname += '/measurements'
+  return redirect(url)
 }
-
-export const handler = postHandler(
-  VitalsSchema,
-  async (_req, ctx: EncounterContext, form_values) => {
-    const patient_id = getRequiredUUIDParam(ctx, 'patient_id')
-
-    const { response, insert } = await promiseProps({
-      response: completeStep(ctx),
-      insert: vitals.insertMeasurements(ctx.state.trx, {
-        patient_id,
-        encounter_id: ctx.state.encounter.encounter_id,
-        encounter_provider_id:
-          ctx.state.encounter_provider.patient_encounter_provider_id,
-        input_measurements: filterOfType(form_values.findings, hasValue),
-      }),
-    })
-
-    assert(insert.success)
-
-    return response
-  },
-)
-
-export async function VitalsPage(ctx: EncounterContext) {
-  const vital_measurements_for_this_encounter = await vitals
-    .measurementsNeededForEncounter(
-      ctx.state.trx,
-      ctx.state.patient,
-    )
-
-  const most_recent_patient_vitals = await patient_findings
-    .getMostRecent(
-      ctx.state.trx,
-      {
-        patient_id: ctx.state.patient.id,
-        snomed_concept_ids: vital_measurements_for_this_encounter.map((o) =>
-          o.snomed_concept_id
-        ),
-      },
-    )
-
-  return (
-    <VitalsForm
-      vital_measurements_for_this_encounter={vital_measurements_for_this_encounter}
-      most_recent_patient_vitals={most_recent_patient_vitals}
-    />
-  )
-}
-
-export default EncounterPage(VitalsPage)
