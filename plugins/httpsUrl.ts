510d6d21e0adbddebb8a874fc709b3f29c549141
Will Weiss <will.weiss1230@gmail.com>
2025-09-15 17:03:05 -0400

    comment

--- plugins/httpsUrl.ts
+++ plugins/httpsUrl.ts
@@ -1,4 +1,11 @@
+/*
+  On production the fresh server itself serves HTTP traffic
+  and a proxy server handles HTTPS.
+  In order to have the ctx.url be the https: address, we overwrite
+  it here
+*/
 import { Plugin } from '$fresh/server.ts'
+import { assert } from 'std/assert/assert.ts'
 
 export default function httpsUrlPlugin(): Plugin {
   return {
@@ -10,6 +17,7 @@ export default function httpsUrlPlugin(): Plugin {
           handler(req, ctx) {
             const proto = req.headers.get('x-forwarded-proto')
             if (proto === 'https' && ctx.url.protocol === 'http:') {
+              assert(req.url.startsWith('http://'))
               ctx.url.protocol = 'https:'
             }
             return ctx.next()
