6159ad99d5af3d395bdab716270d478604e9b27c
Will Weiss <will.weiss1230@gmail.com>
2025-11-02 22:39:38 -0500

    mostly working

--- main.ts
+++ main.ts
@@ -1,6 +1,49 @@
 import { App, staticFiles } from 'fresh'
+import httpsUrlPlugin from './plugins/httpsUrl.ts'
+import { onProduction } from './util/onProduction.ts'
+import { opts as db_opts } from './db/db.ts'
+import { bgRgb8, bold, cyan, rgb8 } from 'std/fmt/colors.ts'
+
+// Pretty print startup info
+const { SERVE_PLAIN_HTTP, PORT } = Deno.env.toObject()
+const protocol = SERVE_PLAIN_HTTP ? 'http:' : 'https:'
+const port = PORT ? parseInt(PORT, 10) : 8000
+const address = `${protocol}//localhost:${port}`
+
+console.log()
+console.log(
+  ' 🩺 ' +
+    bgRgb8(rgb8('Virtual Hospitals Africa ready', 255), 57),
+)
+const is_prod = onProduction()
+if (is_prod) {
+  console.log(
+    `     ` +
+      bgRgb8(rgb8('(running against production)', 255), 59),
+  )
+  console.log()
+}
+console.log(`    ${bold('URL:')} ${cyan(address)}\n`)
+
+if (Deno.env.get('USE_DOCKER_FOR_POSTGRES')) {
+  console.log(
+    ' 💽 ' +
+      bgRgb8(
+        rgb8('Adminer (Postgres client) ready', 255),
+        38,
+      ),
+  )
+  const { database, user, password } = db_opts!
+  const adminer_url =
+    `http://localhost:8888?pgsql=postgres&server=postgres&username=${user}&db=${database}`
+  console.log(`    ${bold('URL:')} ${cyan(adminer_url)}`)
+  console.log(`    ${bold('Password:')} ${password}\n`)
+  console.log()
+}
 
 export const app = new App()
+  // Add HTTPS URL middleware
+  .use(httpsUrlPlugin().middlewares[0].middleware.handler)
   // Add static file serving middleware
   .use(staticFiles())
   // Enable file-system based routing
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import { fresh } from '@fresh/plugin-vite'
 import tailwindcss from '@tailwindcss/vite'
+import { promiseProps } from './util/promiseProps.ts'
+
+const { SERVE_PLAIN_HTTP, PORT } = Deno.env.toObject()
+const httpsOpts: {
+  key?: string
+  cert?: string
+} = SERVE_PLAIN_HTTP ? {} : await promiseProps({
+  key: Deno.readTextFile('./local-certs/localhost.key'),
+  cert: Deno.readTextFile('./local-certs/localhost.crt'),
+})
 
 export default defineConfig({
   plugins: [
     fresh(),
     tailwindcss(),
   ],
+  server: {
+    port: PORT ? parseInt(PORT, 10) : 8000,
+    https: Object.keys(httpsOpts).length > 0 ? httpsOpts : undefined,
+  },
 })
