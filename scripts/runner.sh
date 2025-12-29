222100fe110e3a4ff0c1f5b4a39283aab634a305
Will Weiss <will.weiss1230@gmail.com>
2025-12-28 20:10:06 -0500

    script runner

---
+++ scripts/runner.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+script="$1"
+shift
+
+if [ -f "$script" ]; then
+  :
+elif [ -f "./scripts/$script" ]; then
+  script="./scripts/$script"
+elif [ -f "./scripts/$script.sh" ]; then
+  script="./scripts/$script.sh"
+elif [ -f "./scripts/$script.ts" ]; then
+  script="./scripts/$script.ts"
+else
+  echo "could not find script: $script"
+  exit 1
+fi
+
+if [[ "$script" == *.ts ]]; then
+  deno task run:trusted "$script" "$@"
+else
+  $script "$@"
+fi
