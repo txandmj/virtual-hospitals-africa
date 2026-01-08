4e74bb848adf4fb71c8f1535582d3265c9a65f66
Will Weiss <will.weiss1230@gmail.com>
2026-01-08 12:47:03 -0500

    more findings to look for

--- shared/tasks.ts
+++ shared/tasks.ts
@@ -27,6 +27,16 @@ export const TASKS = [
       (snomed_concept "Chest pain" "finding"))
     (check_for
       (finding ${CLINICAL_FINDING.lang} (snomed_concept "Vomiting" "finding"))))`,
+  `(task
+    (finding ${CLINICAL_FINDING.lang}
+      (snomed_concept "Chest pain" "finding"))
+    (check_for
+      (finding ${CLINICAL_FINDING.lang} (snomed_concept "Pallor of skin of face" "finding"))))`,
+  `(task
+    (finding ${CLINICAL_FINDING.lang}
+      (snomed_concept "Chest pain" "finding"))
+    (check_for
+      (finding ${CLINICAL_FINDING.lang} (snomed_concept "Sweating" "finding"))))`,
 ].map(asTask)
 
 // TODO Separate function for permission around tasks
