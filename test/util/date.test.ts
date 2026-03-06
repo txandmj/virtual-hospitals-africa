d83fd30936f96cd0b2595c25769b5394efb61c41
Will Weiss <will.weiss1230@gmail.com>
2026-03-06 11:11:59 -0500

    test back

--- test/util/date.test.ts
+++ test/util/date.test.ts
@@ -1,15 +1,33 @@
 import { assertEquals } from 'std/assert/assert_equals.ts'
-import { it } from 'std/testing/bdd.ts'
-import { prettyPatientDateOfBirth, formatJohannesburg } from '../../util/date.ts'
+import { formatJohannesburg, prettyPatientDateOfBirth } from '../../util/date.ts'
+import { describe, it } from 'std/testing/bdd.ts'
 
-it('formats a date, day first', () => {
-  const dob = prettyPatientDateOfBirth('1990-03-01')
-  assertEquals(dob, '1 March 1990')
-  throw new Error('x')
-})
+describe('date', () => {
+  describe('prettyPatientDateOfBirth', () => {
+    it('formats a date, day first', () => {
+      const dob = prettyPatientDateOfBirth('1990-03-01')
+      assertEquals(dob, '1 March 1990')
+    })
+  })
+  describe('formatJohannesburg', () => {
+    it('converts between timezones', () => {
+      const formatted = formatJohannesburg('2025-11-07T09:30:00-05:00')
+      assertEquals(formatted, '2025-11-07T16:30:00+02:00')
+    })
 
-it('converts between timezones', () => {
-  const formatted = formatJohannesburg('2025-11-07T09:30:00-05:00')
-  assertEquals(formatted, '2025-11-07T16:30:00+02:00')
-  throw new Error('y')
+    it('gives the start of the day in Johannesburg', () => {
+      const formatted = formatJohannesburg('2025-11-07')
+      assertEquals(formatted, '2025-11-07T00:00:00+02:00')
+    })
+
+    it('handles parsed dates', () => {
+      const formatted = formatJohannesburg({
+        year: '2025',
+        month: '11',
+        day: '7',
+        timezone: 'Africa/Johannesburg',
+      })
+      assertEquals(formatted, '2025-11-07T00:00:00+02:00')
+    })
+  })
 })
