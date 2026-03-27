6ac5643c46fcfcf4951ea90c7664608bdc40c841
Will Weiss <will.weiss1230@gmail.com>
2026-03-26 23:09:33 -0400

    no doctor signoff in script

--- shared/tutorial/script.ts
+++ shared/tutorial/script.ts
@@ -646,7 +646,7 @@ export const TUTORIAL_SCRIPT: ScriptItem[] = [
     type: 'dialogue',
     speaker: 'guide',
     text:
-      'Well done! With your help, Duduzile was able to get diagnosed and treated quickly, potentially saving her life. We were able to get doctor sign off without having to transport the patient saving everyone valuable time.',
+      'Well done! With your help, Duduzile was able to get diagnosed and treated quickly, potentially saving her life.',
   },
   {
     type: 'dialogue',
