0480b8a298d72a78db91704309771fbb21e096f0
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-30 02:07:41 +0200

    Update 29-dizziness.lisp

--- s_expression/system_priority_evaluations/apc-adult/29-dizziness.lisp
+++ s_expression/system_priority_evaluations/apc-adult/29-dizziness.lisp
@@ -6,18 +6,55 @@
   (and
     (clinical_finding (snomed_concept "Dizziness" "finding"))
     (or
-      (clinical_finding (snomed_concept "Chest pain" "finding"))
+      (clinical_finding ( snomed_concept “Weakness of face muscles” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Asymmetry” (qualifier value))
+			AND
+			(clinical_finding ( snomed_concept “Numbness of face” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Asymmetry” (qualifier value))
+			AND
+			(clinical_finding ( snomed_concept “Muscle weakness of upper limb” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Asymmetry” (qualifier value))
+			AND
+			(clinical_finding ( snomed_concept “Numbness of upper limb” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Asymmetry” (qualifier value))
+			AND
+			(clinical_finding ( snomed_concept “Weakness of muscle of lower limb” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Asymmetry” (qualifier value))
+			AND
+			(clinical_finding ( snomed_concept “Numbness of lower limb” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Asymmetry” (qualifier value))
+			AND
+			(clinical_finding ( snomed_concept “Difficulty talking” (finding))
+			OR
+  		(clinical_finding ( snomed_concept “Visual disturbance” (disorder))
+			)
+			(< measurement (clinical_finding ( snomed_concept “Systolic blood pressure” (observable entity) “Millimeter of mercury” (qualifier value) 90))
+			AND
+			(< measurement (clinical_finding ( snomed_concept “Diastolic blood pressure” (observable entity) “Millimeter of mercury” (qualifier value) 60))
+			)
+			(< measurement (clinical_finding ( snomed_concept “Finding of blood glucose level” (finding) “Millimole/liter” (qualifier value) 3))
+			OR
+			(< measurement (clinical_finding ( snomed_concept “Finding of blood glucose level” (finding) “Diabetes mellitus” (disorder) “Known” (qualifier value) “Millimole/liter” (qualifier value) 4))
+			)
+			(clinical_finding (snomed_concept "Chest pain" "finding"))
+			)
       (clinical_finding (snomed_concept "Orthopnea" "finding"))
+			AND
       (clinical_finding (snomed_concept "Leg swelling symptom" "finding"))
-      (clinical_finding (snomed_concept "Injury of head" "disorder"))
-      (clinical_finding (snomed_concept "Unable to stand" "finding"))
-      (clinical_finding (snomed_concept "Nausea and vomiting" "disorder"))
-      (clinical_finding (snomed_concept "Abnormal ocular motility" "finding"))
-      (clinical_finding (snomed_concept "Pulse slow" "finding"))
+			)
+      (clinical_finding (snomed_concept "Injury of head" "disorder") “Recent” (qualifier value))
+			)
+      (clinical_finding (snomed_concept "Unable to stand" "finding") “Unaided” (qualifier value))
+			)
+			(clinical_finding ( snomed_concept “Dizziness” (finding) “New” (qualifier value) “Sudden” (qualifier value) “Severe” (severity modifier) (qualifier value))
+			ANY
+        	(clinical_finding ( snomed_concept “With” (attribute) “Nausea” (finding))
+				  (clinical_finding ( snomed_concept “With” (attribute) “Vomiting” (disorder))
+     		  (clinical_finding (snomed_concept “With” (attribute) "Abnormal ocular motility" "finding"))
+					(clinical_finding ( snomed_concept “With” (attribute) “Abnormal gait” (finding))
+			)
+			(< measurement (clinical_finding ( snomed_concept “Heart rate” (observable entity) “Beats/minute” (qualifier value) 50))
+			OR
       (clinical_finding (snomed_concept "Pulse irregular" "finding"))
+			)
       (< (measurement (snomed_concept "Systolic blood pressure" "observable entity") mmHg) 90)
+			AND
       (< (measurement (snomed_concept "Diastolic blood pressure" "observable entity") mmHg) 60)
-      (< (measurement (snomed_concept "Pulse, function" "observable entity") bpm) 50)
     )
   )
 )
+
