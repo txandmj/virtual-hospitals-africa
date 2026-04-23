3acda0417324c3b2451d19c5d8444524a992705a
Will Weiss <will.weiss1230@gmail.com>
2026-04-23 09:27:15 -0400

    blog header fix

--- db/seed/defs/09_examinations.ts
+++ db/seed/defs/09_examinations.ts
@@ -3,7 +3,7 @@ import { TrxOrDb } from '../../../types.ts'
 import { collect } from '../../../util/inParallel.ts'
 import parseCsv from '../../../util/parseCsv.ts'
 
-export default define(['examinations'], addSeedData, { never_dump: true })
+export default define(['examinations'], addSeedData)
 
 async function addSeedData(trx: TrxOrDb) {
   let order = 0
--- islands/BlogMobileMenu.tsx
+++ islands/BlogMobileMenu.tsx
@@ -1,46 +1,35 @@
-import { Menu } from '@headlessui/react'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon } from '../components/library/icons/heroicons/outline.tsx'
-
-type NavItem = {
-  href: string
-  text: string
-}
-
-const NAV_ITEMS: NavItem[] = [
-  { href: '/', text: 'Home' },
-  { href: '/tutorial', text: 'Try it out' },
-  { href: 'https://virtualhospitalsafrica.org/team', text: 'Team' },
-  { href: '/blog', text: 'Blog' },
-  {
-    href: 'https://github.com/Virtual-Hospitals-Africa/virtual-hospitals-africa',
-    text: 'GitHub',
-  },
-]
+import { NAV_ITEMS } from '../shared/navigation_items.ts'
 
 export default function BlogMobileMenu() {
   return (
-    <Menu as='div' className='relative md:hidden'>
-      <Menu.Button
-        className='flex items-center justify-center w-10 h-10 rounded cursor-pointer text-[#6f7788] hover:text-gray-900'
-        aria-label='Open navigation menu'
-      >
-        <Bars3Icon className='w-6 h-6' />
-      </Menu.Button>
-      <Menu.Items className='absolute right-0 z-20 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'>
-        <div className='py-1'>
-          {NAV_ITEMS.map((item) => (
-            <Menu.Item key={item.href}>
-              <a
-                href={item.href}
-                target={item.href.startsWith('http') ? '_blank' : undefined}
-                className='block px-4 py-2 text-base font-medium text-[#6f7788] data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none'
-              >
-                {item.text}
-              </a>
-            </Menu.Item>
-          ))}
-        </div>
-      </Menu.Items>
-    </Menu>
+    <div className='relative md:hidden'>
+      <Menu>
+        <MenuButton
+          className='flex items-center justify-center w-10 h-10 rounded cursor-pointer text-[#6f7788] hover:text-gray-900 focus:outline-none'
+          aria-label='Open navigation menu'
+        >
+          <Bars3Icon className='w-6 h-6' />
+        </MenuButton>
+        <MenuItems className='absolute right-0 z-20 mt-2 w-52 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'>
+          <div className='py-1'>
+            {NAV_ITEMS.map((item) => (
+              <MenuItem key={item.href}>
+                <a
+                  href={item.href}
+                  target={item.href.startsWith('http') ? '_blank' : undefined}
+                  className={item.cta
+                    ? 'block px-4 py-2 text-base font-medium text-[#473fce] data-[focus]:bg-gray-100 data-[focus]:outline-none'
+                    : 'block px-4 py-2 text-base font-medium text-[#6f7788] data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none'}
+                >
+                  {item.text}
+                </a>
+              </MenuItem>
+            ))}
+          </div>
+        </MenuItems>
+      </Menu>
+    </div>
   )
 }
