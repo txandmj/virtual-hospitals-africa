f3b35b33bdab8e074c93defcef8354e0f11da9f6
Erik Withak <erikaanestad@gmail.com>
2026-04-24 15:30:09 -0700

    dashboard: widget registry

---
+++ components/dashboard/widgets/index.ts
@@ -0,0 +1,10 @@
+import type { WidgetDef } from '../../../util/dashboard/types.ts'
+import { patientsInCareWidget }    from './PatientsInCare.tsx'
+import { encountersInRangeWidget } from './EncountersInRange.tsx'
+import { staffOnShiftWidget }      from './StaffOnShift.tsx'
+
+export const DASHBOARD_WIDGETS: WidgetDef<unknown>[] = [
+  patientsInCareWidget as WidgetDef<unknown>,
+  encountersInRangeWidget as WidgetDef<unknown>,
+  staffOnShiftWidget as WidgetDef<unknown>,
+]
