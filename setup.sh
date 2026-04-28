f17c9ad6e0441a5fa00e65b4ac41562eb42c3f67
Will Weiss <will.weiss1230@gmail.com>
2026-04-28 13:02:47 -0400

    setup.sh

--- setup.sh
+++ setup.sh
@@ -10,18 +10,30 @@ ensure_you_have() {
 }
 
 echo "Checking you have the right tools installed..."
-
 ensure_you_have "git"
 ensure_you_have "deno"
 ensure_you_have "npm"
 ensure_you_have "node"
 ensure_you_have "docker"
 
-echo "Now we'll install the project dependencies..."
+[[ -f db/dumps/snomed ]] || {
+  echo "Did not detect a file db/dumps/snomed"
+  echo "Please run:"
+  echo "  git lfs pull"
+  exit 1
+}
+
+if ! nc -z localhost 5432; then
+  echo "Postgres is not up"
+  echo "Please run:"
+  echo "  docker compose up"
+  exit 1
+fi
 
 deno task switch:docker
 
 echo "Now let's migrate your local database..."
 deno task db:docker reset
 
-echo "Migrations complete! You can now run the server with 'deno task start'"
+echo "Migrations complete! You can now run the server with:"
+echo "  deno task start"
