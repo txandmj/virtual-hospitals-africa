31ffa7bfb2e6aedc76fc2c5837137e296f1f4f9f
Will Weiss <will.weiss1230@gmail.com>
2026-04-30 12:44:32 -0400

    helpful errors

--- routes/s_expression_playground.tsx
+++ routes/s_expression_playground.tsx
@@ -6,14 +6,36 @@ import SExpressionPlayground from '../islands/SExpressionPlayground.tsx'
 import { allConceptsToLookFor, conceptDoesNotExist } from '../test/shared/compiled_s_expressions.test.ts'
 import { stripComments } from '../s_expression/compile.ts'
 
+function countInstances(str: string, needle: string): number {
+  if (needle.length === 0) return 0
+  return str.split(needle).length - 1
+}
+
 export const handler = {
   async POST(ctx: { req: Request }) {
     const { expression } = await ctx.req.json() as { expression?: string }
     if (typeof expression !== 'string' || !expression.trim()) {
       return json({ ok: false, error: 'Expression is empty' })
     }
+    const sans_comments = stripComments(expression)
+    const left_paren_count = countInstances(sans_comments, '(')
+    const right_paren_count = countInstances(sans_comments, ')')
+
+    if (left_paren_count > right_paren_count) {
+      return json({ ok: false, error: 'Unbalanced parentheses, more "(" than ")' })
+    }
+    if (right_paren_count > left_paren_count) {
+      return json({ ok: false, error: 'Unbalanced parentheses, more "(" than ")' })
+    }
+    if (countInstances(sans_comments, '”')) {
+      return json({ ok: false, error: 'Cannot use ” character. Replace with "' })
+    }
+    if (countInstances(sans_comments, '“')) {
+      return json({ ok: false, error: 'Cannot use “ character. Replace with "' })
+    }
+
     try {
-      const node = parseWithSchema(stripComments(expression), any_rule)
+      const node = parseWithSchema(sans_comments, any_rule)
       for (const concept of allConceptsToLookFor(node)) {
         console.log({ concept })
         if (await conceptDoesNotExist({ concept })) {
