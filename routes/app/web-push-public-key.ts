d64db43065cb0ac605e1004e4e8c424d9da62a7b
Chris Du <dwj020724@gmail.com>
2026-06-10 00:19:27 -0700

    Add web push public key route

---
+++ routes/app/web-push-public-key.ts
@@ -0,0 +1,9 @@
+import { vapid_public_key } from '../../external-clients/web-push.ts'
+import { LoggedInHealthWorkerContext } from '../../types.ts'
+import { json } from '../../util/responses.ts'
+
+export const handler = {
+  GET(_ctx: LoggedInHealthWorkerContext) {
+    return json({ public_key: vapid_public_key })
+  },
+}
